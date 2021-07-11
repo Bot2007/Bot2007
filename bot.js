431093cf78c2eb3e5c9c9f25b59ae17f31b07d22
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-12 01:14:30 +0800

    Delete bot.js

--- bot.js
+++
@@ -1,7 +0,0 @@
-let handler = async (m, { usedPrefix }) => m.reply(`Ya saya bot, nak mulakan bot, ketik ${usedPrefix}menu  
-`.trim())
-handler.help = ['bot']
-handler.tags = ['bot']
-handler.command = /^bot$/i
-
-module.exports = handler
