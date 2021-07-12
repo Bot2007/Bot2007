68fe62d21c8f724f8726b178e03730efbad9c87d
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-13 00:42:39 +0800

    Update spammer.js

--- plugins/spammer.js
+++ plugins/spammer.js
@@ -1,24 +1,24 @@
 let handler = async(m, { conn, text }) => {
 let [number, pesan] = text.split `|`
 
-    if (!number) return m.reply(m.chat, 'Silahkan masukan nomor yang akan dispam', m)
-    if (!pesan) return m.reply(m.chat, 'Silahkan masukan pesannya', m)
-    if (text > 500) return conn.reply(m.chat, 'Teks Kepanjangan!', m)
+    if (!number) return m.reply('Silahkan masukan nomor yang akan dispam')
+    if (!pesan) return m.reply('Silahkan masukan pesannya')
+    if (text > 500) return m.reply('Teks Kepanjangan!')
     var nomor = m.sender
-    let spam = `*「 WA SPAMMER 」*\n\nDari : wa.me/${nomor.split("@s.whatsapp.net")[0]}\nPesan : ${pesan}\n\n~BOT_STYLE`
+    let spam = `*「 WA SPAMMER 」*\n\nDari : wa.me/${nomor.split("@s.whatsapp.net")[0]}\nPesan : ${pesan}\n\nTaufik`.trim()
 
-    conn.sendMessage(${number}@s.whatsapp.net, spam, m)
-    conn.sendMessage(${number}@s.whatsapp.net, spam, m)
-    conn.sendMessage(${number}@s.whatsapp.net, spam, m)
-    conn.sendMessage(${number}@s.whatsapp.net, spam, m)
-    conn.sendMessage(${number}@s.whatsapp.net, spam, m)
-    conn.sendMessage(${number}@s.whatsapp.net, spam, m)
-    conn.sendMessage(${number}@s.whatsapp.net, spam, m)
-    conn.sendMessage(${number}@s.whatsapp.net, spam, m)
-    conn.sendMessage(${number}@s.whatsapp.net, spam, m)
-    conn.sendMessage(${number}@s.whatsapp.net, spam, m)
+    conn.sendMessage(''${number}@s.whatsapp.net', spam, m)
+    conn.sendMessage('${number}@s.whatsapp.net', spam, m)
+    conn.sendMessage('${number}@s.whatsapp.net', spam, m)
+    conn.sendMessage('${number}@s.whatsapp.net', spam, m)
+    conn.sendMessage('${number}@s.whatsapp.net', spam, m)
+    conn.sendMessage('${number}@s.whatsapp.net', spam, m)
+    conn.sendMessage('${number}@s.whatsapp.net', spam, m)
+    conn.sendMessage('${number}@s.whatsapp.net', spam, m)
+    conn.sendMessage('${number}@s.whatsapp.net', spam, m)
+    conn.sendMessage('${number}@s.whatsapp.net', spam, m)
     let logs = `[!] Berhasil mengirim spam wa ke nomor ${korban} 10 kali`
-    conn.reply(m.chat, logs, m)
+    m.reply(logs.trim())
 }
 handler.help = ['spam <nomor|pesan>', 'spamwa <nomor|pesan>']
 handler.tags = ['spammer']
