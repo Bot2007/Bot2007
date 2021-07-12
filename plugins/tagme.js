fa660fd91621ff4e9c14ee48eca25605e5172d8d
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-13 02:50:41 +0800

    Update tagme.js

--- plugins/tagme.js
+++ plugins/tagme.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, text, participants }) => {
+let handler = async (m, { text }) => {
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   m.reply(text + '\n' + `@${who.replace(/@.+/, '')}`.trim(), null, {
     contextInfo: { [who] }
