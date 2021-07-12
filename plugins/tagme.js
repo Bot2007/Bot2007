d0d0305772bb9f8b597b47bbf0b4371cd1626087
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-13 04:32:57 +0800

    Update tagme.js

--- plugins/tagme.js
+++ plugins/tagme.js
@@ -1,7 +1,7 @@
 let handler = async (m, { text }) => {
   let who = m.fromMe ? conn.user.jid : m.sender
   let mentionedJid = [who]
-  m.reply('Nih gw tag buat lo ' + `@${who.replace(/@.+/, '')}`.trim(), null, {
+  m.reply(`@${who.replace(/@.+/, '')}`.trim(), null, {
     contextInfo: { mentionedJid }
   })
 }
