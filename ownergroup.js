ed9a8897c57fe359e9e60739e8720ca79dc28f05
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-13 04:36:10 +0800

    Delete ownergroup.js

--- ownergroup.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { text }) => {
-  let who = m.chat.split`-`[0]
-  let mentionedJid = [m.chat.split`-`[0]]
-  m.reply(`@${who.replace(/@.+/, '')}`.trim(), null, {
-    contextInfo: { mentionedJid }
-  })
-}
-handler.command = ['ownergroup']
-module.exports = handler
