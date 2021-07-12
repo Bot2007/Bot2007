e615158f0a8aa742e2a78554c1c597053818e49b
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-13 02:30:20 +0800

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,6 +1,4 @@
-let { MessageType } = require('@adiwajshing/baileys')
-
-let handler = async (m, { conn, text, participants }) => {
+let handler = async (m, { text, participants }) => {
   let users = participants.map(u => u.jid)
   m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
     contextInfo: { mentionedJid: users }
