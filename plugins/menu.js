1c385f0303a05330f2829280f67a204ee4f409cf
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-14 00:52:20 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -157,7 +157,7 @@ let TaufikComel = conn
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).join`|`})`, 'g'), (_, name) => replace[name])
-  TaufikComel.sendMessage(m.chat, text.trim(), m, null, { contextInfo: { forwardingScore: 508, isForwarded: true, "expiration": 10, "externalAdReply": {
+  m.reply(text.trim(), null, { contextInfo: { forwardingScore: 508, isForwarded: true, "expiration": 10, "externalAdReply": {
                   "title": "Taufik Bot",
                   "body": "MENU TAUFIK BOT",
                   "mediaType": "VIDEO",
