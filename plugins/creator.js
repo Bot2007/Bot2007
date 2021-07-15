c1628455e9bedefca00c848fdc89e9306886d4c4
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-15 21:36:56 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,10 +1,8 @@
 let fs = require ('fs')
-let handler  = async (m, { from, text, conn }) => {
+let handler  = async (m, { from, conn }) => {
 const fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: `1625623054@broadcast`, }, message: { "contactMessage": { "displayName": `Ini owner/bot ku`, "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:XL;Taufik,;;;\nFN:Taufik Bot,\nitem1.TEL;waid=601173093564:601173093564\nitem1.X-ABLabel:Ponsel\nEND:VCARD`, "jpegThumbnail": fs.readFileSync('./src/TaufikBot.png')}}}
 conn.sendContact(m.chat, '601173093564', 'Taufik HENSEM', null, {contextInfo: {
-    mentionedJid: m.sender }, quoted: fkon})
-   .then((res) => conn.sendMessage(m.chat, 'Nih kontak ownerku', m, false, {contextInfo: {
-    mentionedJid: m.sender }, quoted: res}))
+    mentionedJid: m.sender }, quoted: fkon}).then((res) => m.reply(`Ini ownerku yang tercinta`.trim(), null, {quoted: res}))
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
