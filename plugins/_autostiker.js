c7f827f319eb842e81afb211f9522d047ae9aebb
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-19 03:13:08 +0800

    Update _autostiker.js

--- plugins/_autostiker.js
+++ plugins/_autostiker.js
@@ -1,31 +1,29 @@
 const { MessageType } = require('@adiwajshing/baileys')
 const { sticker } = require('../lib/sticker')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+let handler = async (m, { conn }) => {
     let chat = global.DATABASE.data.chats[m.chat]
     if (chat.stiker && !chat.isBanned) {
-    let stiker = false
-  try {
-    let q = m
-    let mime = (q.msg || q).mimetype || ''
-    if (/image/.test(mime)) {
-      let img = await q.download()
-      if (!img) throw false
-      stiker = await sticker(img, false, global.packname, global.author)
-    } else if (/video/.test(mime)) {
-      if ((q.msg || q).seconds > 11) return m.reply('Maksimal 10 detik!')
-      let img = await q.download()
-      if (!img) throw false 
-      stiker = await sticker(img, false, global.packname, global.author)
-    } else if (/webp/.test(mime)) {
-      let img = await q.download()
-      if (!img) throw false
-      stiker = await sticker(img, false, global.packname, global.author)
+        // try {
+        let q = m
+        let mime = (q.msg || q).mimetype || ''
+        if (/webp/.test(mime)) return
+        if (/image/.test(mime)) {
+            let img = await q.download().catch(_ => _)
+            if (!img) throw false
+            stiker = await sticker(img, false, global.packname, global.author).catch(_ => _)
+            await conn.sendMessage(m.chat, stiker, MessageType.sticker, { quoted: m }).catch(_ => _)
+        } if (/video/.test(mime)) {
+            if ((q.msg || q).seconds > 11) throw 'Maksimal 10 detik!'
+            let img = await q.download()
+            if (!img) throw false
+            stiker = await sticker(img, false, global.packname, global.author).catch(_ => _)
+            await conn.sendMessage(m.chat, stiker, MessageType.sticker, { quoted: m }).catch(_ => _)
+        }
+        // } finally {
+        //     if (stiker) {
+        //     }
+        // }
     }
-  } finally {
-    if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, {
-      quoted: m
-    })}
     return true
-}
 }
 module.exports = handler
