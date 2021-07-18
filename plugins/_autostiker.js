c7fcb5d5b3933b25eb7ad980022f295a6c4e1a04
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-19 03:26:17 +0800

    Update _autostiker.js

--- plugins/_autostiker.js
+++ plugins/_autostiker.js
@@ -4,20 +4,20 @@ let handler = async (m, { conn }) => {
     let chat = global.DATABASE.data.chats[m.chat]
     if (chat.stiker && !chat.isBanned) {
         // try {
-        let q = m
-        let mime = (q.msg || q).mimetype || ''
-        if (/webp/.test(mime)) return
-        if (/image/.test(mime)) {
-            let img = await q.download().catch(_ => _)
-            if (!img) throw false
-            stiker = await sticker(img, false, global.packname, global.author).catch(_ => _)
-            await conn.sendMessage(m.chat, stiker, MessageType.sticker, { quoted: m }).catch(_ => _)
-        } if (/video/.test(mime)) {
-            if ((q.msg || q).seconds > 11) throw 'Maksimal 10 detik!'
-            let img = await q.download()
-            if (!img) throw false
-            stiker = await sticker(img, false, global.packname, global.author).catch(_ => _)
-            await conn.sendMessage(m.chat, stiker, MessageType.sticker, { quoted: m }).catch(_ => _)
+    let sticker = false  
+    let q = m
+    let mime = (q.msg || q).mimetype || ''
+    if (/image/.test(mime)) {
+      let img = await q.download()
+      if (!img) throw false
+      stikero = await sticker(img, false, global.packname, global.author)
+      await conn.sendMessage(m.chat, stikero, MessageType.sticker, { quoted: m })
+    } if (/video/.test(mime)) {
+      if ((q.msg || q).seconds > 11) return m.reply('Maksimal 10 detik!')
+      let img = await q.download()
+      if (!img) throw false
+      stiker = await sticker(img, false, global.packname, global.author)
+      await conn.sendMessage(m.chat, stiker, MessageType.sticker, { quoted: m })
         }
         // } finally {
         //     if (stiker) {
