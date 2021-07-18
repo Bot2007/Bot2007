461c284f24c2e1466ff16d7724d51e02ffbdb212
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-19 02:36:29 +0800

    Update _autostiker.js

--- plugins/_autostiker.js
+++ plugins/_autostiker.js
@@ -5,19 +5,28 @@ let handler = m => m
 handler.before = async function (m) {
     let chat = global.DATABASE.data.chats[m.chat]
     if (chat.stiker && !chat.isBanned) {
-        let stiker = false
-  try {
-    let q = m.quoted ? m.quoted : m
-    let mime = (q.msg || q).mimetype || ''
-    if (/image|video/.test(mime)) {
-      let img = await q.download()
-      if (!img) throw 'Foto/Video tidak ditemukan'
-      stiker = await sticker(img, false, global.packname, global.author)
-    } else if (args[0]) stiker = await sticker(false, args[0], global.packname, global.author)
-  } finally {
-    conn.sendMessage(m.chat, stiker, MessageType.sticker, {
-      quoted: m
-    })}
+        // try {
+        let sticker = false
+        let q = m.quoted ? m.quoted : m
+        let mime = (q.msg || q).mimetype || ''
+        if (/webp/.test(mime)) return
+        if (/image/.test(mime)) {
+            let img = await q.download()
+            if (!img) throw false
+            stiker = await sticker(img, false, global.packname, global.author).catch(_ => _)
+            await this.sendMessage(m.chat, stiker, MessageType.sticker, { quoted: m }).catch(_ => _)
+        } if (/video/.test(mime)) {
+            if ((q.msg || q).seconds > 11) throw 'Maksimal 10 detik!'
+            let img = await q.download()
+            if (!img) throw false
+            stiker = await sticker(img, false, global.packname, global.author).catch(_ => _)
+            await this.sendMessage(m.chat, stiker, MessageType.sticker, { quoted: m }).catch(_ => _)
+        }
+        // } finally {
+        //     if (stiker) {
+        //     }
+        // }
     }
+    return true
 }
 module.exports = handler
