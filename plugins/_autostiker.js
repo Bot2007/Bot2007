8f7d12357676a60a9c8c5de89368f594c879913a
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-19 02:43:57 +0800

    Update _autostiker.js

--- plugins/_autostiker.js
+++ plugins/_autostiker.js
@@ -6,21 +6,18 @@ handler.before = async function (m) {
     let chat = global.DATABASE.data.chats[m.chat]
     if (chat.stiker && !chat.isBanned) {
         // try {
-        let sticker = false
-        let q = m.quoted ? m.quoted : m
-        let mime = (q.msg || q).mimetype || ''
-        if (/webp/.test(mime)) return
-        if (/image/.test(mime)) {
-            let img = await q.download()
-            if (!img) throw false
-            stiker = await sticker(img, false, global.packname, global.author)
-            await this.sendMessage(m.chat, stiker, MessageType.sticker, { quoted: m })
-        } if (/video/.test(mime)) {
-            if ((q.msg || q).seconds > 11) throw 'Maksimal 10 detik!'
-            let img = await q.download()
-            if (!img) throw false
-            stiker = await sticker(img, false, global.packname, global.author)
-            await this.sendMessage(m.chat, stiker, MessageType.sticker, { quoted: m })
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || ''
+    if (/image/.test(mime)) {
+      let img = await q.download()
+      if (!img) throw `balas gambar dengan caption *${usedPrefix + command}*`
+      stiker = await sticker(img, false, global.packname, global.author)
+    } else if (/video/.test(mime)) {
+      if ((q.msg || q).seconds > 11) return m.reply('Maksimal 10 detik!')
+      let img = await q.download()
+      if (!img) throw `balas video/gif dengan caption *${usedPrefix + command}*`
+      stiker = await sticker(img, false, global.packname, global.author)
+     await this.sendMessage(m.chat, stiker, MessageType.sticker, { quoted: m })
         }
         // } finally {
         //     if (stiker) {
