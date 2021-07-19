06b97525274c5e3e022ee5b816d848318b355be0
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-19 23:48:53 +0800

    Update jawabsalam.js

--- plugins/jawabsalam.js
+++ plugins/jawabsalam.js
@@ -1,7 +1,5 @@
 let handler  = async (m, { conn }) => { 
   m.reply('Waalaikumsalam')
-  let Waalaikumsalam = './src/Waalaikumsalam.mp3'
-  conn.sendFile(m.chat, Waalaikumsalam, 'salam.opus', null, m, true)
   conn.chatRead(m.chat)
 }
 handler.help = ['Jawabsalam']
