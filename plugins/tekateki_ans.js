ad16647f488745bb805f604087d8d584ca029406
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-20 01:52:15 +0800

    Update tekateki_ans.js

--- plugins/tekateki_ans.js
+++ plugins/tekateki_ans.js
@@ -1,7 +1,7 @@
 let handler = m => m
 handler.before = async function (m) {
   let id = m.chat
-  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*tbhint/i.test(m.quoted.text)) return !0
+  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*tete/i.test(m.quoted.text)) return !0
   conn.tekateki = conn.tekateki ? conn.tekateki : {}
   if (!(id in conn.tekateki)) return m.reply('Soal itu telah berakhir')
   if (m.quoted.id == conn.tekateki[id][0].id) {
