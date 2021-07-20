250f027f20421a1c151a8990408a3fcc5d7e1176
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-20 08:44:21 +0800

    Update cekexpired.js

--- plugins/cekexpired.js
+++ plugins/cekexpired.js
@@ -1,5 +1,5 @@
 let handler = async m => 
-if (global.DATABASE.data.chats[m.chat].expired = 0) throw `Maap, owner tidak menetapkan expired untuk group ini`.trim()
+if (global.DATABASE.data.chats[m.chat].expired = 0) throw `Maap owner tidak menetapkan expired untuk group ini`.trim()
 m.reply(`Expired pada ${msToDate(global.DATABASE.data.chats[m.chat].expired - now)}`.trim()) 
 handler.help = ['cekluput','cekexpired','kapanexpired','kapanluput']
 handler.tags = ['main']
