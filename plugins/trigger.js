bc326e203644ed57f9fb7496d423c5eb3e29ef6c
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-21 07:37:09 +0800

    Create trigger.js

---
+++ plugins/trigger.js
@@ -0,0 +1,36 @@
+const uploadImage = require('../lib/uploadImage') 
+const { sticker } = require('../lib/sticker')
+const { MessageType } = require('@adiwajshing/baileys')
+
+let handler = async (m, { conn, text }) => {
+ try {
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) throw 'Tidak ada foto'
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
+  let img = await q.download()
+  let url = await uploadImage(img)
+  let trigger = `https://some-random-api.ml/canvas/triggered?avatar=${url}`
+  let stiker = await sticker(null, trigger, 'Sticker Buatan', 'Taufik')
+  conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+    quoted: m
+  })
+ } catch (e) {
+   m.reply('Conversion Failed')
+  }
+}
+handler.help = ['trigger']
+handler.tags = ['sticker']
+handler.command = /^trigger$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
