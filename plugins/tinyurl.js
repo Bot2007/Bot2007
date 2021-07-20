46a9819c2ea9435f3750e1093bf74a7100351935
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-20 23:08:24 +0800

    Update tinyurl.js

--- plugins/tinyurl.js
+++ plugins/tinyurl.js
@@ -2,12 +2,13 @@ let axios = require('axios')
 let handler = async (m, { conn, usedPrefix, args }) => {
   if (!args[0]) throw `
 Tidak ada url
-Contoh penggunaan: ${usedPrefix}tinyurl https://youtube.com`.trim()
+Contoh penggunaan: 
+${usedPrefix}tinyurl https://youtube.com`.trim()
   let url = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
   axios.get(`https://tinyurl.com/api-create.php?url=${url}`).then ((res) => {
   let hasil = `
 nih urlnya, *${res.data}*
-Noted: jika nggak bisa mungkin errorya.`.trim()
+Noted: jika nggak bisa masuk ke urlnya, mungkin errorya ;)`.trim()
   m.reply(hasil)})
 }
 handler.help = ['tinyurl'].map(v => v + ' <url>')
