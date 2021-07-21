40ab0ba94b01918fa79f5aa3d39bfd3f34ca37e1
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-21 10:08:01 +0800

    Create lirik2.js

---
+++ plugins/lirik2.js
@@ -0,0 +1,26 @@
+let { MessageType } = require('@adiwajshing/baileys')
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args, usedPrefix, DevMode }) => {
+    try {
+        if (!args || !args[0] || args.length < 1) return m.reply('Judul lagunya apa om?')
+        let res = await fetch(global.API('bg', '/lirik', { 
+            title: args[0],
+            artist: args[1] || '' 
+        }))
+        let json = await res.json()
+        if (json.status !== true) throw json
+        m.reply(`
+*Lyrics ${args[0]}*
+\`\`\`${json.result}\`\`\`
+`.trim())
+    } catch (e) {
+        m.reply('Error!!')
+        console.log(e)
+    }
+}
+    
+handler.help = ['lirik2', 'lyrics2'].map(v => ' [title] [artist]')
+handler.tags = ['internet']
+handler.command = /^(l(irik2|yrics2))$/i
+
+module.exports = handler
