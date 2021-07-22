39c649225bea0857accd64f436c9ecec43fb1067
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-22 15:35:09 +0800

    Update daily.js

--- plugins/daily.js
+++ plugins/daily.js
@@ -1,15 +1,16 @@
 let { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn }) => {
+    const prems = 5000
+    const free = 500
     let user = global.DATABASE._data.users[m.sender]
-    let __timers = (new Date - user.lastclaim)
+    let __timers = (new Date - user.lastdaily)
     let _timers = (86400000 - __timers)
     let timers = clockString(_timers) 
-    if (new Date - user.lastclaim > 86400000) {
-        conn.reply(m.chat, `Anda sudah mengklaim dan mendapatkan 1000 💵money dan 1 potion`, m)
-        global.DATABASE._data.users[m.sender].money += 1000
-        global.DATABASE._data.users[m.sender].potion += 1
-        global.DATABASE._data.users[m.sender].lastclaim = new Date * 1
+    if (new Date - user.lastdaily > 86400000) {
+        conn.reply(m.chat, `+{isPrems ? prem : free} XP`, m)
+        global.DATABASE._data.users[m.sender].exp += isPrems ? prem : free
+        global.DATABASE._data.users[m.sender].lastdaily = new Date * 1
     } else {
         let buttons = button(`silahkan tunggu *🕒${timers}* lagi untuk bisa mengclaim lagi`, user)
         conn.sendMessage(m.chat, buttons, MessageType.buttonsMessage, { quoted: m })
@@ -46,7 +47,8 @@ function clockString(ms) {
 function button(teks, user) {
     const buttons = []
     
-    let claim = new Date - user.lastclaim > 86400000
+    let daily = new Date - user.lastdaily > 86400000
+    let claim = new Date - user.claim > 86400000
     let monthly = new Date - user.lastmonthly > 2592000000
     let weekly = new Date - user.lastweekly > 604800000
     console.log({claim, monthly, weekly})
@@ -54,11 +56,12 @@ function button(teks, user) {
     if (monthly) buttons.push({buttonId: `id${buttons.length + 1}`, buttonText: {displayText: '/monthly'}, type: 1})
     if (weekly) buttons.push({buttonId: `id${buttons.length + 1}`, buttonText: {displayText: '/weekly'}, type: 1})
     if (claim) buttons.push({buttonId: `id${buttons.length + 1}`, buttonText: {displayText: '/claim'}, type: 1})
+    if (daily) buttons.push({buttonId: `id${buttons.length + 1}`, buttonText: {displayText: '/daily'}, type: 1})
     if (buttons.length == 0) throw teks
     
     const buttonMessage = {
         contentText: teks,
-        footerText: '©games-wabot',
+        footerText: '@taufikmusa07',
         buttons: buttons,
         headerType: 1
     }
