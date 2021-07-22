04500b9d85484b778d0663caf52fb670cfb3f79e
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-22 21:36:27 +0800

    Update tinyurl.js

--- plugins/tinyurl.js
+++ plugins/tinyurl.js
@@ -3,13 +3,17 @@ let handler = async (m, { usedPrefix, args }) => {
   if (!args[0]) throw `
 Tidak ada url
 Contoh penggunaan: 
-${usedPrefix}tinyurl https://youtube.com`.trim()
+${usedPrefix}tinyurl https://youtube.com
+
+Credit: @${global.conn.user.jid.replace(/@.+/, '')}`.trim()
   let url = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
   axios.get(`https://tinyurl.com/api-create.php?url=${url}`).then ((res) => {
   let hasil = `
 nih urlnya, *${res.data}*
-Noted: jika nggak bisa masuk ke urlnya, mungkin errorya ;)`.trim()
-  m.reply(hasil)})
+Noted: jika nggak bisa masuk ke urlnya, mungkin errorya ;)
+
+Credit: @${global.conn.user.jid.replace(/@.+/, '')}`.trim()
+  m.reply(m.chat, hasil, m, { contextInfo: { mentionedJid: global.conn.user.jid } })})
 }
 handler.help = ['tinyurl'].map(v => v + ' <url>')
 handler.tags = ['internet']
