655a6425b36020fa4d6d1727565e83154283bde6
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-23 16:41:09 +0800

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -4,7 +4,6 @@ let handler = async (m, { conn, text }) => {
   let who
   if (text.includes('@0')) who = '0@s.whatsapp.net'
   else if (m.isGroup) who = cm.participant = m.mentionedJid[0]
-  else who = m.chat
   if (!who) throw 'Tag salah satu lah'
   txt = text.replace('@' + who.split`@`[0], '').trimStart()
   return conn.sendContact(m.chat, who, txt || conn.getName(who), m)
