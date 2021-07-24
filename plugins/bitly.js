6ab0e7b4267d92c317b81283f1070d26b3f2a8df
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-24 15:41:59 +0800

    Update bitly.js

--- plugins/bitly.js
+++ plugins/bitly.js
@@ -7,13 +7,13 @@ ${usedPrefix}bitly https://youtube.com
 
 Credit: @${global.conn.user.jid.replace(/@.+/, '')}`.trim()
   let url = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
-  let res = await axios.get(`https://tobz-api.herokuapp.com/api/bitly?url=${url}&apikey=LRBOuIZYf9ozZmu1wAkf`)
+  axios.get(`https://tobz-api.herokuapp.com/api/bitly?url=${urls}&apikey=Tobzzz17`).then ((res) => {
   let hasil = `
 nih urlnya, *${res.data.result}*
 Noted: jika nggak bisa masuk ke urlnya, mungkin errorya ;)
 
 Credit: @${global.conn.user.jid.replace(/@.+/, '')}`.trim()
-  m.reply(m.chat, hasil, m, { contextInfo: { mentionedJid: global.conn.user.jid } })
+  m.reply(m.chat, hasil, m, { contextInfo: { mentionedJid: global.conn.user.jid } })})
 }
 handler.help = ['bitly'].map(v => v + ' <url>')
 handler.tags = ['internet']
