34e31fe403e693315204db307e166891cbd7ae7f
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-25 02:36:49 +0800

    Update _tagowner.js

--- plugins/_tagowner.js
+++ plugins/_tagowner.js
@@ -1,6 +1,7 @@
 let handler = m => m
-handler.before = m => {
-  let jids = [...new Set([...([601173093564@s.whatsapp.net] || [])])]
+handler.before = (m, { conn }) => {
+  let mentionedJid = [global.conn.user.jid]
+  let jids = [...new Set([...(mentionedJid || [])])]
   for (let jid of jids) {
      m.reply(`Apa sih tag tag ownerku`.trim())
   }
