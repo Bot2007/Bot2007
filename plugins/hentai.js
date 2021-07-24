cf92959503cfd2d28e07744242881d2a6ad8a373
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-24 17:38:39 +0800

    Update hentai.js

--- plugins/hentai.js
+++ plugins/hentai.js
@@ -4,7 +4,7 @@ let url = await fetch('https://waifu.pics/api/nsfw/neko')
   let hentai = await url.json()
   let hasil = `Link ${hentai.url}`
  // conn.fakeReply(m.chat, '*_Sabar sedang mengirim hentai..._*','0@s.whatsapp.net','Orang sabar di sayang mantan:)')
-conn.sendFile(m.chat, ardiganz.url, 'hentai.png', hasil, m, false, { thumbnail: require('fs').readFileSync('src/TaufikBot.png') } )
+conn.sendFile(m.chat, hentai.url, 'hentai.png', hasil, m, false, { thumbnail: require('fs').readFileSync('src/TaufikBot.png') } )
 }
 handler.command = /^hentai$/i
 handler.register = true
