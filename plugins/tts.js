bf6c07a73af826fd22a2243d43c36da8a083f5fa
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-25 07:18:49 +0800

    Update tts.js

--- plugins/tts.js
+++ plugins/tts.js
@@ -8,9 +8,11 @@ let handler = async (m, { conn, usedPrefix, args }) => {
   let er = `Format anda salah
 
 Contoh penggunaan:
-${usedPrefix}tts id Taufik hensem terlajak
+${usedPrefix}tts id Taufik hensem
+${usedPrefix}tts id <reply teks>
+${usedPrefix}tts Taufik hensem (Languange is Indonesia)
+${usedPrefix}tts <reply teks>  (Languange is Indonesia)
     `.trim()
-    if (!args[0]) throw er
   let lang = args[0]
   let text = args.slice(1).join(' ')
   if ((args[0] || '').length !== 2) {
@@ -20,19 +22,18 @@ ${usedPrefix}tts id Taufik hensem terlajak
   let eo = `Teksnya mana?
     `.trim()
     if (!text) throw eo
-  if (!text && m.quoted && m.quoted.text) text = eo
+  if (!m.quoted && m.quoted.text) throw er
 
   let res 
   try { res = await tts(text, lang) }
   catch (e) {
-  m.reply('Kode bahasa anda salah. Ketik .kodebahasa untuk melihat kode bahasa yang support. Jadi kami terpaksa menggunakan bahasa Indonesia.')
-  res = await tts(text)
+  m.reply('Kode bahasa anda salah. Ketik ${usedPrefix}kodebahasa untuk melihat kode bahasa yang support.')
   } finally { 
     m.reply('Sedang merekam')
   conn.sendFile(m.chat, res, 'tts.opus', null, m, true)
   }
 }
-handler.help = ['tts <lang> <teks>']
+handler.help = ['tts <lang> <teks>', 'tts <reply teks>']
 handler.tags = ['tools']
 handler.command = /^g?tts$/i
 module.exports = handler
