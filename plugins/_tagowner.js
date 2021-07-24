0c8f1a87fbacec8578eeb7a9afe4b9c01c3ed165
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-25 02:13:03 +0800

    Delete _tagowner.js

--- plugins/_tagowner.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-  let jids = [...new Set([...(m.mentionedJid[601173093564] || [])])]
-  for (let jid of jids) {
-    m.reply(`Apa sih tag tag ownerku`.trim())
-  }
-  return true
-}
-
-module.exports = handler
