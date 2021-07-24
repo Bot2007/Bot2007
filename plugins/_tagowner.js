9f5f61c8d53f8a9d215f1420994bd537c3a29f95
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-24 22:58:39 +0800

    Update _tagowner.js

--- plugins/_tagowner.js
+++ plugins/_tagowner.js
@@ -1,9 +1,7 @@
 let handler = m => m
 handler.before = m => {
 let jids = [...new Set([...(m.mentionedJid || [])])]
-  for (let jid of jids) {
-    let user = global.DATABASE.data.users[global.conn.user.jid]
-    if (!user) continue
+  for (let global.conn.user.jid of jids) {
     m.reply(`Apa sih tag tag ownerku`.trim())
   }
   return true
