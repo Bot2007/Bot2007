2926386c8fa676867685fe71f0fc8aae70d4926a
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-24 15:43:42 +0800

    Update ass.js

--- plugins/ass.js
+++ plugins/ass.js
@@ -2,9 +2,8 @@ let axios = require('axios');
 let fetch = require('node-fetch')
 let neko = require('nekos.life')
 let Neko = new neko()
-     let handler  = async (m, { conn, args }) => {
-if (!DATABASE.data.chats[m.chat].nsfw && m.isGroup) throw 'Feature Nsfw Disable\nType *!enable* *nsfw* to activate this feature'
-     json = (await axios.get('https://meme-api.herokuapp.com/gimme/animebooty')).data
+let handler  = async (m, { conn, args }) => {
+   json = (await axios.get('https://meme-api.herokuapp.com/gimme/animebooty')).data
    conn.sendFile(m.chat, json.url, 'ass.jpg', '', m, false)
 }
 handler.help = ['ass']
@@ -15,7 +14,8 @@ handler.mods = false
 handler.premium = false
 handler.group = false
 handler.private = false
-handler.limit = true
+
+handler.limit = true
 handler.admin = false
 handler.botAdmin = false
 
