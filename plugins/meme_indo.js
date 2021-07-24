a65f580ec3955c8a8799477bdadf3d2b93c1d7ed
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-24 17:19:53 +0800

    Update meme_indo.js

--- plugins/meme_indo.js
+++ plugins/meme_indo.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-  let url = await fetch(global.API('https://api.zeks.xyz', '/api/memeindo', {}, 'apikey')
+  let url = await fetch('https://api.zeks.xyz/api/memeindo?apikey=')
   let barbar = await url.json()
 conn.sendFile(m.chat, barbar.result, 'memeindo', JSON.stringify(barbar, null, 2), m)
 }
