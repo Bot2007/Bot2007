3ef4a1e66967a359d4726cf54d2c55ad8e580404
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-26 05:22:33 +0800

    Create buttonmenu.js

---
+++ plugins/buttonmenu.js
@@ -0,0 +1,22 @@
+let handler = async (m,{ conn }) => let menu = conn.prepareMessageFromContent(m.chat, {
+                    "listMessage":  {
+                        "title": "@taufikmusa07",
+                        "description": "MENU BOT",
+                        "buttonText": "Menu disini",
+                        "listType": "SINGLE_SELECT",
+                        "sections": [
+                            {
+                                "rows": [
+                                    {
+                                        "title": `MENU BO TAUFIK`,
+                                        "rowId": ".menubot"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                }, {})
+            conn.relayWAMessage(menu, {waitForAck: true})
+handler.command = /^(menu)$/i
+
+module.exports = handler
