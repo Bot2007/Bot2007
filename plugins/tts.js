066e49c0da39e36f661d4a107166297ccc6f8e94
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-25 08:43:19 +0800

    Update tts.js

--- plugins/tts.js
+++ plugins/tts.js
@@ -26,8 +26,7 @@ ${usedPrefix}tts <reply teks>  (Languange is Indonesia)
   if (!text && m.quoted && m.quoted.text) text = m.quoted.text
 
   let res = await tts(text, lang)
-  conn.sendFile(m.chat, res, 'tts.opus', null, m, true).catch(e =>
-  m.reply('Kode bahasa anda salah. Ketik .kodebahasa untuk melihat kode bahasa yang support.'))
+  conn.sendFile(m.chat, res, 'tts.opus', null, m, true).catch(m.reply('Kode bahasa anda salah. Ketik .kodebahasa untuk melihat kode bahasa yang support.'))
 }
 handler.help = ['tts <lang> <teks>', 'tts <reply teks>']
 handler.tags = ['tools']
