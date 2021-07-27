6946592a9439b34b3de4a94172f1c82435e9bc14
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-27 23:26:33 +0800

    Update happymod.js

--- plugins/happymod.js
+++ plugins/happymod.js
@@ -10,10 +10,7 @@ let handler = async(m, { conn, text }) => {
                 const modo = happymod.data.result[0]
                 const resmod = `• *Title* : ${modo.title}\n• *Purchase* : ${modo.purchase}\n• *Size* : ${modo.size}\n• *Root* : ${modo.root}\n• *Version* : ${modo.version}\n• *Price* : ${modo.price}\n• *Link* : ${modo.link}\n• *Download* : ${modo.download}`
                 aruga.sendFileFromUrl(from, modo.image, 'HAPPYMOD.jpg', resmod, id)
-            } catch (err) {
-                console.log(err)
-            }
-           
+            }       
 handler.help = ['happymod']
 handler.tags = ['downloader']
 handler.command = /^(happymod)$/i
