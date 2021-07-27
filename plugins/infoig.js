5e84d04e4dbfdb117abdc99207f89e2303dd7f22
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-27 23:49:19 +0800

    Update infoig.js

--- plugins/infoig.js
+++ plugins/infoig.js
@@ -1,9 +1,8 @@
-let axios = require("axios");
-let handler = async(m, { conn, text }) => { 
-
-  await m.reply('*[❗] WAIT, Tunggu Sebentar*') 
-     axios.get(`https://alfians-api.herokuapp.com/api/stalk?username=${teks}`).then ((res) =>{
+let axios = require('axios');
+let handler = async (m, { conn, text }) => {
+  axios.get(`https://alfians-api.herokuapp.com/api/stalk?username=${teks}`).then ((res) => {
   let hasil = `BIODATA INSTAGRAM ATAS NAMA _${teks}_ \n\n *Username* : _${res.data.Username}_ \n *Nama* : _${res.data.Name}_ \n *Jumlah Followers* : _${res.data.Jumlah_Followers}_ \n *Jumlah Following* : _${res.data.Jumlah_Following}_ \n *Jumlah Post* : _${res.data.Jumlah_Post}_ `
+  conn.sendFile(m.chat, '${res.data.Profile_pic}', 'Ok.png', hasil.trim(), m)
 
 handler.help = ['infoig']
 handler.tags = ['internet']
