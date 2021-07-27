88cf13c7e567d79746bcf2e821bdfb24fca5e386
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-27 23:36:24 +0800

    Update moddroid.js

--- plugins/moddroid.js
+++ plugins/moddroid.js
@@ -11,10 +11,7 @@ let handler = async(m, { conn, text }) => {
                 let modo = moddroid.data.result[0]
                 let resmod = `• *Title* : ${modo.title}\n• *Publisher* : ${modo.publisher}\n• *Size* : ${modo.size}\n• *MOD Info* : ${modo.mod_info}\n• *Version* : ${modo.latest_version}\n• *Genre* : ${modo.genre}\n• *Link* : ${modo.link}\n• *Download* : ${modo.download}`
                 conn.sendFileFromUrl(from, modo.image, 'MODDROID.jpg', resmod, id)
-            } catch (err) {
-                console.log(err)
             }
-
 handler.help = ['moddroid']
 handler.tags = ['downloader']
 handler.command = /^(moddroid)$/i
