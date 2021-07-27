505ca9dc12d16760e9fa9dcc32819a94ba5869b4
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-27 23:50:43 +0800

    Update infoig.js

--- plugins/infoig.js
+++ plugins/infoig.js
@@ -1,4 +1,4 @@
-let axios = require('axios');
+let axios = require('axios')
 let handler = async (m, { conn, text }) => {
   axios.get(`https://alfians-api.herokuapp.com/api/stalk?username=${teks}`).then ((res) => {
   let hasil = `BIODATA INSTAGRAM ATAS NAMA _${teks}_ \n\n *Username* : _${res.data.Username}_ \n *Nama* : _${res.data.Name}_ \n *Jumlah Followers* : _${res.data.Jumlah_Followers}_ \n *Jumlah Following* : _${res.data.Jumlah_Following}_ \n *Jumlah Post* : _${res.data.Jumlah_Post}_ `
