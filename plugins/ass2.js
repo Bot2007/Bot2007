0cd7bd1860535ea17fb5f8eeeff296945d23d133
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-27 22:39:34 +0800

    Update and rename ass.js to ass2.js

--- plugins/ass2.js
+++ plugins/ass2.js
@@ -6,9 +6,9 @@ let handler  = async (m, { conn, args }) => {
    json = (await axios.get('https://meme-api.herokuapp.com/gimme/animebooty')).data
    conn.sendFile(m.chat, json.url, 'ass.jpg', '', m, false)
 }
-handler.help = ['ass']
+handler.help = ['ass2']
 handler.tags = ['sange']
-handler.command = /^ass$/i
+handler.command = ['ass2']
 handler.owner = false
 handler.mods = false
 handler.premium = false
