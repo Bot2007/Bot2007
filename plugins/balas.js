d132f39f576f959dc5b13e43f4cf6209dfe9e475
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-27 08:51:24 +0800

    Update balas.js

--- plugins/balas.js
+++ plugins/balas.js
@@ -2,8 +2,9 @@ let { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     let [ nomor, kata ] = text.split` `
     if (!nomor && !kata || isNaN(nomor)) throw 'Masukkan Nomor dan kata. Contoh: ${usedPrefix + command} 601173093564 Hai Taufik hensem'
-    m.reply(nomor + '@s.whatsapp.net', 'Balasan dari ${kata}', m)
-    await m.reply('Mesej anda berjaya dihantar')
+    m.reply(nomor + '@s.whatsapp.net', `Balasan dari ${m.chat.replace(/@.+/, '')} untuk anda. 
+${kata}`.trim(), m)
+    await m.reply('Mesej anda berjaya dihantar ke Nomor ${nomor}')
 }
 handler.help = ['balas'].map(v => v + ' [nomor] [teks]')
 handler.tags = ['owner']
@@ -14,6 +15,3 @@ handler.mods = true
 handler.fail = null
 
 module.exports = handler
-
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
