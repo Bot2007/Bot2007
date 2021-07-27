e638b13ceaa76ea2815afc1ca4f20064ede09f94
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-27 21:11:59 +0800

    Update charainfo.js

--- plugins/charainfo.js
+++ plugins/charainfo.js
@@ -4,14 +4,15 @@ let handler = async(m, { conn, text }) => {
   let res = await fetch(global.API('https://api.jikan.moe', '/v3/search/character', { q: text }))
   if (!res.ok) throw await res.text()
   let json = await res.json()
-  let { name, alternative_names, url, image_url } = json.results[0]
-let charaingfo = `*Name:* ${name}
-*Nickname:* ${alternative_names}
-*Link*: ${url}`
+  let { name, alternative_names, url, image_url, type } = json.results[0]
+let charaingfo = `💬 *Name:* ${name}
+💭 *Nickname:* ${alternative_names}
+🔗 *Link*: ${url}
+👤 *Character Type*: ${type}`
+
   conn.sendFile(m.chat, image_url, '', charaingfo, m)
 }
 handler.help = ['character <nama>']
-handler.tags = ['anime']
+handler.tags = ['internet']
 handler.command = /^(chara|character)$/i
-//kyaa jangan biarkan wabot-aq terbengkalai sampai nurutomo kembali
 module.exports = handler
