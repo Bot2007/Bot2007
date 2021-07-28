9cdb45e4e702514000ab66fb57fadf69c5b8772b
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-28 16:57:12 +0800

    Create bitly.js

---
+++ plugins/bitly.js
@@ -0,0 +1,13 @@
+let fetch = require('node-fetch')
+let handler = async (m, { text }) => {
+  if (!text) throw 'Linknya mana'
+  let res = await fetch(global.API('xteam', '/shorturl/bitly', { url: text }, 'APIKEY'))
+  let json = await res.json()
+  if (json.status) m.reply(json.result.link + '\n' + `Credit: Taufik`.trim())
+  else throw 'Link invalid'
+}
+handler.help = ['bitly'].map(v => v + ' <url>')
+handler.tags = ['tools']
+handler.command = /^bitly$/i
+// Made By Taufik
+module.exports = handler
