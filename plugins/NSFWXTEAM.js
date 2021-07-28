a7fec0b3af1702c7e1a4a06c7422905cac9ffedb
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-28 15:49:18 +0800

    Update NSFWXTEAM.js

--- plugins/NSFWXTEAM.js
+++ plugins/NSFWXTEAM.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, command }) => {
   m.reply('Sedang Diproses...')
-  let res = 'https://api.xteam.xyz/randomimage/${command}?APIKEY=MIMINETBOT'
+  let res = `https://api.xteam.xyz/randomimage/${command}?APIKEY=MIMINETBOT`
   conn.sendFile(m.chat, res, 'nsfw.jpg', `tobat woi`, m, false)
 }
 handler.help = ['ass','panties','pussy','nsfwneko','mstrb','jahy','hentai','cuckold']
@@ -11,8 +11,8 @@ handler.command = /^(ass|panties|pussy|nsfwneko|hentai|mstrb|jahy|cuckold)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
-handler.group = true
-handler.private = false
+handler.group = false
+handler.private = true
 // Original made by Taufik
 handler.admin = false
 handler.botAdmin = false
