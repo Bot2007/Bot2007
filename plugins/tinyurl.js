5c80060b32ca2aa9b92ab3a73bb583fbbeaecf5b
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-28 16:55:48 +0800

    Update tinyurl.js

--- plugins/tinyurl.js
+++ plugins/tinyurl.js
@@ -3,7 +3,7 @@ let handler = async (m, { text }) => {
   if (!text) throw 'Linknya mana'
   let res = await fetch(global.API('xteam', '/shorturl/tinyurl', { url: text }, 'APIKEY'))
   let json = await res.json()
-  if (json.status) m.reply(json.result + '\n' + `Credit: @${global.conn.user.jid.replace(/@.+/, '')}`.trim(), null, { contextInfo: { mentionedJid: global.conn.user.jid } })
+  if (json.status) m.reply(json.result + '\n' + `Credit: Taufik`.trim())
   else throw 'Link invalid'
 }
 handler.help = ['tinyurl'].map(v => v + ' <url>')
