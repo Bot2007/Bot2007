df3e21656462f7e68730364ed6a8d8670c2132ac
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-07-31 17:10:35 +0800

    Update nyampah.js

--- plugins/nyampah.js
+++ plugins/nyampah.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn }) => {
-    let __timers = (new Date - global.DATABASE._data.users[m.sender].lastclaim)
+    let __timers = (new Date - global.DATABASE._data.users[m.sender].lastnyampah)
     let _timers = (43200000 - __timers)
     let timers = clockString(_timers) 
     let user = global.DATABASE._data.users[m.sender]
-    if (new Date - global.DATABASE._data.users[m.sender].lastclaim > 43200000) {
+    if (new Date - global.DATABASE._data.users[m.sender].lastnyampah > 43200000) {
     conn.reply(m.chat, `Kamu Menjadi Tukang Sampah Dan Kamu Mendapat *20000* MONEY`, m)
     user.money += 20000
-        user.lastclaim = new Date * 0
+        user.lastnyampah = new Date * 0
     } else conn.reply(m.chat, `Silahkan Menunggu *${timers}* Lagi Untuk Bisa Bekerja Lagi`, m)
 }
 handler.help = ['nyampah']
