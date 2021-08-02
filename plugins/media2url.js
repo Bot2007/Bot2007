ed653bdcd8ac8a825bc18e48e33c93d7c6889b99
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-03 06:11:06 +0800

    Update media2url.js

--- plugins/media2url.js
+++ plugins/media2url.js
@@ -3,7 +3,7 @@ const uploadImage = require('../lib/uploadImage')
 
  let handler  = async (m, { conn, usedPrefix, command }) => {
   let q = m.quoted ? m.quoted : m
-      let img = await q.msg.download()
+      let img = await q.download()
       if (!img) throw 'balas foto/video dengan caption *${usedPrefix + command}*'
       try {
       urlfile = await uploadImage(img)
