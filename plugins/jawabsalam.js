ee367e2347aca2b5a7eda91bacbaaab563244e5f
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-04 15:21:56 +0800

    Update jawabsalam.js

--- plugins/jawabsalam.js
+++ plugins/jawabsalam.js
@@ -2,8 +2,6 @@ let handler  = async (m, { conn }) => {
   m.reply('Waalaikumsalam')
   conn.chatRead(m.chat)
 }
-handler.help = ['Jawabsalam']
-handler.tags = ['kerang']
 handler.customPrefix = /aslm|assalam/i
 handler.command = new RegExp
 module.exports = handler
