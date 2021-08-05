41997b1aa300520af7be31a26c8c5c941bf81a8b
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-06 04:56:16 +0800

    Update shorturl3.js

--- plugins/shorturl3.js
+++ plugins/shorturl3.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text, command }) => {
   if (!text) throw 'Linknya mana'
-  let res = await fetch(global.API('xteam', '/shorturl/${command}', { url: text }, 'APIKEY'))
+  let res = await fetch(global.API('xteam', '/shorturl/' + command, { url: text }, 'APIKEY'))
   let json = await res.json()
   if (json.status) m.reply(json.result.shorturl + '\n' + `Credit: Taufik`.trim())
   else throw 'Link invalid'
