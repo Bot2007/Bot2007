5dff294c9dde1b5a717b4467c6df6fc673fd9199
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-06 04:38:20 +0800

    Create shorturl1.js

---
+++ plugins/shorturl1.js
@@ -0,0 +1,10 @@
+let fetch = require('node-fetch')
+let handler = async (m, { text, command }) => {
+  if (!text) throw 'Linknya mana'
+  let res = await fetch(global.API('xteam', '/shorturl/${command}', { url: text }, 'APIKEY'))
+  let json = await res.json()
+  if (json.status) m.reply(json.result + '\n' + `Credit: Taufik`.trim())
+  else throw 'Link invalid'
+}
+handler.command = /^(tinyurl|shorturlat)$/i
+module.exports = handler
