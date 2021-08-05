abb9ee7d7d7428f8acd402e95778ffd91722922d
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-05 18:28:27 +0800

    Update _autostiker.js

--- plugins/_autostiker.js
+++ plugins/_autostiker.js
@@ -1,32 +1,35 @@
 const { MessageType } = require('@adiwajshing/baileys')
 const { sticker } = require('../lib/sticker')
+const uploadFile = require('../lib/uploadFile')
+const uploadImage = require('../lib/uploadImage')
+let { webp2png } = require('../lib/webp2mp4')
 let handler = m => m
 
 handler.before = async function (m) {
     let chat = global.DATABASE.data.chats[m.chat]
     if (chat.stiker && !chat.isBanned && !m.fromMe && !m.isBaileys) {
-        try {
         let q = m
+        let stiker = false
         let mime = (q.msg || q).mimetype || ''
         if (/webp/.test(mime)) return
         if (/image/.test(mime)) {
-        let img = await q.download()
-        if (!img) return
-        stiker = await sticker(img, false, global.packname, global.author)
+            let img = await q.download()
+            let link = await uploadImage(img)
+            if (!img) return
+            stiker = await sticker(0, link, global.packname, global.author)
         } else if (/video/.test(mime)) {
-        if ((q.msg || q).seconds > 11) return m.reply('Maksimal 10 detik!')
-        let img = await q.download()
-        if (!img) return
-        stiker = await sticker(img, false, global.packname, global.author)
+            if ((q.msg || q).seconds > 11) return m.reply('Maksimal 10 detik!')
+            let img = await q.download()
+            let link = await uploadFile(img)
+            if (!img) return
+            stiker = await sticker(0, link, global.packname, global.author)
         } else if (m.text) {
             if (isUrl(m.text)) stiker = await sticker(false, m.text.split` `[0], global.packname, global.author)
             else return
         }
-        } finally {
-    if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, {
-      quoted: m
-    })
-         }
+        if (stiker) await this.sendMessage(m.chat, stiker, 'stickerMessage', {
+            quoted: m
+        })
     }
     return true
 }
