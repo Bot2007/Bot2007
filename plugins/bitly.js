bd71501ef0ea7f52a27a47c46d171afd24bf305e
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-06 04:13:59 +0800

    Update bitly.js

--- plugins/bitly.js
+++ plugins/bitly.js
@@ -6,7 +6,7 @@ let handler = async (m, { text }) => {
   if (json.status) m.reply(json.result.link + '\n' + `Credit: Taufik`.trim())
   else throw 'Link invalid'
 }
-handler.help = ['bitly','tinyurl','shorturlat','customgg <nama yang mau dicustom>'].map(v => v + ' <url>')
+handler.help = ['bitly','tinyurl','shorturlat','customgg <nama yang mau dicustom>', 'sid', 'cuttly'].map(v => v + ' <url>')
 handler.tags = ['shorturl']
 handler.command = /^bitly$/i
 // Made By Taufik
@@ -36,3 +36,14 @@ let handler = async (m, { text, command }) => {
 handler3.command = /^customcuttly$/i
 
 module.exports = handler3
+
+let handler = async (m, { text, command }) => {
+  if (!text) throw 'Linknya mana'
+  let res = await fetch(global.API('xteam', '/shorturl/${command}', { url: text }, 'APIKEY'))
+  let json = await res.json()
+  if (json.status) m.reply(json.result.shorturl + '\n' + `Credit: Taufik`.trim())
+  else throw 'Link invalid'
+}
+handler4.command = /^(sid|cuttly)$/i
+// Made By Taufik
+module.exports = handler4
