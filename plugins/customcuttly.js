39dbf759a8389eb478b0d89f53b78ce88db38c44
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-06 04:58:21 +0800

    Update and rename shorturl2.js to customcuttly.js

--- plugins/customcuttly.js
+++ plugins/customcuttly.js
@@ -1,9 +1,9 @@
 let fetch = require('node-fetch')
-let handler = async (m, { text, command }) => {
+let handler = async (m, { text }) => {
   let [TEKS, NAMA] = text.split`|`
   if (!text) throw 'Linknya mana\nContoh penggunaan:\n .customcuttly https://youtube.com|linkyoutube'
   if (!NAMA) throw 'Format salah\nContoh penggunaan:\n .customcuttly https://youtube.com|linkyoutube'
-  let res = await fetch(global.API('xteam', '/shorturl/${command}', { url: TEKS, nama: NAMA }, 'APIKEY'))
+  let res = await fetch(global.API('xteam', '/shorturl/customcuttly', { url: TEKS, nama: NAMA }, 'APIKEY'))
   let json = await res.json()
   if (json.status) m.reply(json.result + '\n' + `Credit: Taufik`.trim())
   else throw 'Link invalid'
