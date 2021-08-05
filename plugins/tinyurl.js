4ebf975da1fd040bac1ae20ab8b963f7be763781
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-06 03:37:21 +0800

    Update tinyurl.js

--- plugins/tinyurl.js
+++ plugins/tinyurl.js
@@ -1,13 +1,13 @@
 let fetch = require('node-fetch')
-let handler = async (m, { text }) => {
+let handler = async (m, { text, command }) => {
   if (!text) throw 'Linknya mana'
-  let res = await fetch(global.API('xteam', '/shorturl/tinyurl', { url: text }, 'APIKEY'))
+  let res = await fetch(global.API('xteam', '/shorturl/${command}', { url: text }, 'APIKEY'))
   let json = await res.json()
   if (json.status) m.reply(json.result + '\n' + `Credit: Taufik`.trim())
   else throw 'Link invalid'
 }
-handler.help = ['tinyurl'].map(v => v + ' <url>')
+handler.help = ['tinyurl','shorturlat'].map(v => v + ' <url>')
 handler.tags = ['short']
-handler.command = /^tinyurl$/i
+handler.command = /^(tinyurl|shorturlat)$/i
 
 module.exports = handler
