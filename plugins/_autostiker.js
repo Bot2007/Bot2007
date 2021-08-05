2632b72a4dc553396ca30616f768dc14a83011f3
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-05 18:20:49 +0800

    Update _autostiker.js

--- plugins/_autostiker.js
+++ plugins/_autostiker.js
@@ -11,12 +11,12 @@ handler.before = async function (m) {
         if (/webp/.test(mime)) return
         if (/image/.test(mime)) {
         let img = await q.download()
-        if (!img) throw `balas gambar dengan caption *${usedPrefix + command}*`
+        if (!img) return
         stiker = await sticker(img, false, global.packname, global.author)
         } else if (/video/.test(mime)) {
         if ((q.msg || q).seconds > 11) return m.reply('Maksimal 10 detik!')
         let img = await q.download()
-        if (!img) throw `balas video/gif dengan caption *${usedPrefix + command}*`
+        if (!img) return
         stiker = await sticker(img, false, global.packname, global.author)
         } else if (m.text) {
             if (isUrl(m.text)) stiker = await sticker(false, m.text.split` `[0], global.packname, global.author)
