7531552a53149a9ba641277e76d85754713153ef
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-06 04:43:34 +0800

    Create shorturl2.js

---
+++ plugins/shorturl2.js
@@ -0,0 +1,15 @@
+let fetch = require('node-fetch')
+let handler = async (m, { text, command }) => {
+  let [TEKS, NAMA] = text.split`|`
+  if (!text) throw 'Linknya mana\nContoh penggunaan:\n .customcuttly https://youtube.com|linkyoutube'
+  if (!NAMA) throw 'Format salah\nContoh penggunaan:\n .customcuttly https://youtube.com|linkyoutube'
+  let res = await fetch(global.API('xteam', '/shorturl/${command}', { url: TEKS, nama: NAMA }, 'APIKEY'))
+  let json = await res.json()
+  if (json.status) m.reply(json.result + '\n' + `Credit: Taufik`.trim())
+  else throw 'Link invalid'
+}
+handler.help = ['customcuttly'].map(v => v + ' <url>|<nama yang mau dicustom>')
+handler.tags = ['shorturl']
+handler.command = /^customcuttly$/i
+
+module.exports = handler
