e321b8cab7e9b818b3a3073c092b8f0f622171c7
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-05 18:10:53 +0800

    Update _autostiker.js

--- plugins/_autostiker.js
+++ plugins/_autostiker.js
@@ -6,21 +6,20 @@ handler.before = async function (m) {
     let chat = global.DATABASE.data.chats[m.chat]
     if (chat.stiker && !chat.isBanned && !m.fromMe && !m.isBaileys) {
         try {
-    let q = m
-        let stiker = false
+        let q = m
         let mime = (q.msg || q).mimetype || ''
         if (/webp/.test(mime)) return
         if (/image/.test(mime)) {
             let img = await q.download()
             let link = await uploadImage(img)
             if (!img) return
-            stiker = await sticker(0, link, global.packname, global.author)
+            stiker = await sticker(false, link, global.packname, global.author)
         } else if (/video/.test(mime)) {
             if ((q.msg || q).seconds > 11) return m.reply('Maksimal 10 detik!')
             let img = await q.download()
             let link = await uploadFile(img)
             if (!img) return
-            stiker = await sticker(0, link, global.packname, global.author)
+            stiker = await sticker(false, link, global.packname, global.author)
         } else if (m.text) {
             if (isUrl(m.text)) stiker = await sticker(false, m.text.split` `[0], global.packname, global.author)
             else return
