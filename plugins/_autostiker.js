79ae5fb46f0cb8e68aa6f66de41d445fff65cacb
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-05 18:03:48 +0800

    Update _autostiker.js

--- plugins/_autostiker.js
+++ plugins/_autostiker.js
@@ -7,23 +7,32 @@ handler.before = async function (m) {
     if (chat.stiker && !chat.isBanned && !m.fromMe && !m.isBaileys) {
         try {
     let q = m
-    let mime = (q.msg || q).mimetype || ''
-    if (/image/.test(mime)) {
-      let img = await q.download()
-      stiker = await sticker(img, false, global.packname, global.author)
-    } else if (/video/.test(mime)) {
-      if ((q.msg || q).seconds > 11) return m.reply('Maksimal 10 detik!')
-      let img = await q.download()
-      stiker = await sticker(img, false, global.packname, global.author)
-    } else if (args[0]) {
-      if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
-      else return false
-    }
+        let stiker = false
+        let mime = (q.msg || q).mimetype || ''
+        if (/webp/.test(mime)) return
+        if (/image/.test(mime)) {
+            let img = await q.download()
+            let link = await uploadImage(img)
+            if (!img) return
+            stiker = await sticker(0, link, global.packname, global.author)
+        } else if (/video/.test(mime)) {
+            if ((q.msg || q).seconds > 11) return m.reply('Maksimal 10 detik!')
+            let img = await q.download()
+            let link = await uploadFile(img)
+            if (!img) return
+            stiker = await sticker(0, link, global.packname, global.author)
+        } else if (m.text) {
+            if (isUrl(m.text)) stiker = await sticker(false, m.text.split` `[0], global.packname, global.author)
+            else return
+        }
+        if (stiker) await this.sendMessage(m.chat, stiker, MessageType.sticker, {
+            quoted: m
+        })
   } finally {
     if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, {
       quoted: m
     })
-    else return false
+    else return
          }
     }
     return true
