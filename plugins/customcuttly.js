0ecb1c454535321bb2c9702efe3b575e7e1358e0
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-06 05:22:55 +0800

    Update customcuttly.js

--- plugins/customcuttly.js
+++ plugins/customcuttly.js
@@ -5,7 +5,8 @@ let handler = async (m, { text }) => {
   if (!NAMA) throw 'Format salah\nContoh penggunaan:\n .customcuttly https://youtube.com|linkyoutube'
   let res = await fetch(global.API('xteam', '/shorturl/customcuttly', { url: TEKS, nama: NAMA }, 'APIKEY'))
   let json = await res.json()
-  if (json.status) return m.reply(json.result + '\n' + `Credit: Taufik`.trim())
+  if (json.status = true) return m.reply(json.result + '\n' + `Credit: Taufik`.trim())
+  if (json.status = false) m.reply(json.result + '\n' + `Credit: Taufik`.trim())
 }
 handler.help = ['customcuttly'].map(v => v + ' <url>|<nama yang mau dicustom>')
 handler.tags = ['shorturl']
