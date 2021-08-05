a14aeea755618f965637c1f70e9e12be4578dc96
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-06 04:34:21 +0800

    Update shorturlxteam.js

--- plugins/shorturlxteam.js
+++ plugins/shorturlxteam.js
@@ -7,43 +7,8 @@ let handler = async (m, { text }) => {
   if (json.status) m.reply(json.result.link + '\n' + `Credit: Taufik`.trim())
   else throw 'Link invalid'
 }
-handler.help = ['bitly','tinyurl','shorturlat','customgg <nama yang mau dicustom>', 'sid', 'cuttly'].map(v => v + ' <url>')
+handler.help = ['bitly'].map(v => v + ' <url>')
 handler.tags = ['shorturl']
 handler.command = /^bitly$/i
 // Made By Taufik
 module.exports = handler
-
-let handler2 = async (m, { text, command }) => {
-  if (!text) throw 'Linknya mana'
-  let res = await fetch(global.API('xteam', '/shorturl/${command}', { url: text }, 'APIKEY'))
-  let json = await res.json()
-  if (json.status) m.reply(json.result + '\n' + `Credit: Taufik`.trim())
-  else throw 'Link invalid'
-}
-handler2.command = /^(tinyurl|shorturlat)$/i
-
-module.exports = handler2
-
-let handler3 = async (m, { text, command }) => {
-  let [NAMA, TEKS] = text.split` `
-  if (!text) throw 'Linknya mana\nContoh penggunaan:\n .customcuttly linkyoutube|https://youtube.com'
-  if (!TEKS) throw 'Format salah\nContoh penggunaan:\n .customcuttly linkyoutube|https://youtube.com'
-  let res = await fetch(global.API('xteam', '/shorturl/${command}', { url: TEKS, nama: NAMA }, 'APIKEY'))
-  let json = await res.json()
-  if (json.status) m.reply(json.result + '\n' + `Credit: Taufik`.trim())
-  else throw 'Link invalid'
-}
-handler3.command = /^customcuttly$/i
-
-module.exports = handler3
-
-let handler4 = async (m, { text, command }) => {
-  if (!text) throw 'Linknya mana'
-  let res = await fetch(global.API('xteam', '/shorturl/${command}', { url: text }, 'APIKEY'))
-  let json = await res.json()
-  if (json.status) m.reply(json.result.shorturl + '\n' + `Credit: Taufik`.trim())
-  else throw 'Link invalid'
-}
-handler4.command = /^(sid|cuttly)$/i
-// Made By Taufik
-module.exports = handler4
