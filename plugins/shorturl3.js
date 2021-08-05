81425229d0652c59746fff962a94666fa39e36de
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-06 04:45:22 +0800

    Create shorturl3.js

---
+++ plugins/shorturl3.js
@@ -0,0 +1,14 @@
+let fetch = require('node-fetch')
+let handler = async (m, { text, command }) => {
+  if (!text) throw 'Linknya mana'
+  let res = await fetch(global.API('xteam', '/shorturl/${command}', { url: text }, 'APIKEY'))
+  let json = await res.json()
+  if (json.status) m.reply(json.result.shorturl + '\n' + `Credit: Taufik`.trim())
+  else throw 'Link invalid'
+}
+handler.help = ['sid', 'cuttly'].map(v => v + ' <url>')
+handler.tags = ['shorturl']
+handler.command = /^(sid|cuttly)$/i
+// Made By Taufik
+module.exports = handler
+© 
