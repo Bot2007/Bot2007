e4c069fd2f79022af741cb73bd19f10eb331bce5
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-06 03:31:54 +0800

    Update bitly.js

--- plugins/bitly.js
+++ plugins/bitly.js
@@ -7,7 +7,7 @@ let handler = async (m, { text }) => {
   else throw 'Link invalid'
 }
 handler.help = ['bitly'].map(v => v + ' <url>')
-handler.tags = ['tools']
+handler.tags = ['shorturl']
 handler.command = /^bitly$/i
 // Made By Taufik
 module.exports = handler
