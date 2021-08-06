6ea2f3d45e79510e0de74baba06323a2a4044fcf
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-06 17:48:45 +0800

    Update daily.js

--- plugins/daily.js
+++ plugins/daily.js
@@ -12,8 +12,7 @@ let handler = async (m, { conn, isPrems}) => {
         global.DATABASE._data.users[m.sender].exp += isPrems ? prem : free
         global.DATABASE._data.users[m.sender].lastdaily = new Date * 1
     } else {
-        let buttons = button(`silahkan tunggu *🕒${timers}* lagi untuk bisa mengclaim lagi`, user)
-        conn.sendMessage(m.chat, buttons, MessageType.buttonsMessage, { quoted: m })
+        conn.send3Button(m.chat, `silahkan tunggu *🕒${timers}* lagi untuk bisa mengclaim lagi`.trim(), 'Pilih yang mana', 'CLAIM', '.claim', 'WEEKLY', '.weekly', 'MONTHLY', '.monthly')
     }
 }
 handler.help = ['daily']
@@ -42,29 +41,4 @@ function clockString(ms) {
   let s = Math.floor(ms / 1000) % 60
   console.log({ms,h,m,s})
   return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
-}
-
-function button(teks, user) {
-    const buttons = []
-    
-    let claim = new Date - user.lastclaim > 86400000
-    let daily = new Date - user.lastdaily > 86400000
-    let monthly = new Date - user.lastmonthly > 2592000000
-    let weekly = new Date - user.lastweekly > 604800000
-    console.log({claim, daily, monthly, weekly})
-    
-    if (monthly) buttons.push({buttonId: `id${buttons.length + 1}`, buttonText: {displayText: '.monthly'}, type: 1})
-    if (weekly) buttons.push({buttonId: `id${buttons.length + 1}`, buttonText: {displayText: '.weekly'}, type: 1})
-    if (claim) buttons.push({buttonId: `id${buttons.length + 1}`, buttonText: {displayText: '.claim'}, type: 1})
-    if (daily) buttons.push({buttonId: `id${buttons.length + 1}`, buttonText: {displayText: '.daily'}, type: 1})
-    if (buttons.length == 0) throw teks
-    
-    const buttonMessage = {
-        contentText: teks,
-        footerText: '@taufikmusa07',
-        buttons: buttons,
-        headerType: 1
-    }
-    
-    return buttonMessage
 }
