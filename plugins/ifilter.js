bc008b19eb366ca37e88458dc4f354635c5d50a6
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-06 19:41:14 +0800

    Update ifilter.js

--- plugins/ifilter.js
+++ plugins/ifilter.js
@@ -3,13 +3,24 @@ let fetch = require('node-fetch')
 const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn, text }) => {
+  m.reply('*Sedang di proses, Mohon tunggu sebentar kak:p*')
  try {
- if (!text) return m.reply(`masukin filternya!!
-
-Cara penggunaan!! reply image atau send image dengan caption *#ifilter lofi*
-
+ if (!text) throw 'masukin filternya!!'
+//  let [teks, teks2] = text.split('|')
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) throw 'Tidak ada foto'
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
+  let img = await q.download()
+  let url = await uploadImage(img)
+  heum = await fetch(global.API('lol', `/api/filter/${text}`, { img: url }, 'apikey'))
+  json = await heum.buffer()
+   conn.sendMessage(m.chat, heum, MessageType.image, {
+    quoted: m, caption: `*Filter: ${text}*\nFollow instagram instagram.com/taufikmusa07`
+  })
+ } catch (e) {
+   m.reply(`Cara penggunaan!!\n reply image atau send image dengan caption *#ifilter lofi*
 [ *List Filter* ]
-
 lofi
 1977
 aden
@@ -36,23 +47,10 @@ valencia
 walden
 willow
 xpro2
-
 _note:_
 masukan parameter dengan benar!
-Jika error berarti lagi error:v`.trim())
-  let q = m.quoted ? m.quoted : m
-  let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw 'Tidak ada foto'
-  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
-  let img = await q.download()
-  let url = await uploadImage(img)
-  heum = await fetch(global.API('lol', `/api/filter/` + text, { img: url }, 'apikey'))
-  json = await heum.buffer()
-   conn.sendMessage(m.chat, heum, MessageType.image, {
-    quoted: m, caption: `*Filter: ${text}*\nFollow instagram instagram.com/caliph_71`
-  })
- } catch (e) {
-   m.reply(`Error`)
+Jika error berarti lagi error:v`)
+  }
 }
 handler.help = ['ifilter']
 handler.tags = ['tools']
