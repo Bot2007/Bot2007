08dcad1a308332c15c332b7166e1cbbd0bc78ce6
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-06 19:35:52 +0800

    Update ifilter.js

--- plugins/ifilter.js
+++ plugins/ifilter.js
@@ -4,7 +4,7 @@ const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn, text }) => {
  try {
- if (!text) throw `masukin filternya!!
+ if (!text) return m.reply(`masukin filternya!!
 
 Cara penggunaan!! reply image atau send image dengan caption *#ifilter lofi*
 
@@ -39,7 +39,7 @@ xpro2
 
 _note:_
 masukan parameter dengan benar!
-Jika error berarti lagi error:v`.trim()
+Jika error berarti lagi error:v`.trim())
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
   if (!mime) throw 'Tidak ada foto'
