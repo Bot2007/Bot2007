d948753f0d043bc0906dddb873a5b25b162a1204
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-09 04:49:52 +0800

    Delete testingm.js

--- plugins/testingm.js
+++
@@ -1,9 +0,0 @@
-let handler = async m => m.reply(`
-Owner: ${global.conn.user.jid}
-Anda: ${m.sender}
-`.trim()) //
-handler.help = ['testingm']
-handler.tags = ['info']
-handler.command = /^testingm$/i
-
-module.exports = handler
