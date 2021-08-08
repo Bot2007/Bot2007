7fd93ea84f6408407c1a0acde5e83d56d447ec74
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-09 05:19:39 +0800

    Update tekateki2_hint.js

--- plugins/tekateki2_hint.js
+++ plugins/tekateki2_hint.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn }) => {
-    conn.tekateki = conn.tekateki ? conn.tekateki : {}
+    conn.tekateki2 = conn.tekateki2 ? conn.tekateki2 : {}
     let id = m.chat
-    if (!(id in conn.tekateki)) throw false
-    let json = conn.tekateki[id][1]
+    if (!(id in conn.tekateki2)) throw false
+    let json = conn.tekateki2[id][1]
     let nya = json.jawaban
     let nyanya = nya.replace(/[bcdfghjklmnpqrstvwxyz]/g, '_')
     m.reply('```' + nyanya + '```')
