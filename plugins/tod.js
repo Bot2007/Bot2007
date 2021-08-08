5b87c0bad9988c3a97851ed645b5c90b25f29fb8
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-08 16:26:57 +0800

    Update tod.js

--- plugins/tod.js
+++ plugins/tod.js
@@ -1,26 +1,26 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, command }) => {
   if (/^tod$/i.test(command)) {
-    conn.send3Button(m.chat, 'Truth or Dare', 'made with ❤️ by ariffb', 'TRUTH', ',truth', 'DARE', ',dare', 'RANDOM', `${pickRandom([',dare', ',truth'])}`)
+    conn.send3Button(m.chat, 'Truth or Dare', 'made with ❤️ by Taufik', 'TRUTH', '.truth3', 'DARE', '.dare2', 'RANDOM', `${pickRandom([',dare', ',truth'])}`)
   }
-  if (/^truth$/i.test(command)) {
+  if (/^truth3$/i.test(command)) {
     let res = await fetch(global.API('pencarikode', '/api/truthid', {}, 'apikey'))
     if (!res.ok) throw await `${res.status} ${res.statusText}`
     let json = await res.json()
     if (json.message == "") throw json
-    conn.send3Button(m.chat, json.message, '', 'TRUTH', ',truth', 'DARE', ',dare', 'RANDOM', `${pickRandom([',dare', ',truth'])}`)
+    conn.send3Button(m.chat, json.message, '', 'TRUTH', '.truth3', 'DARE', '.dare2', 'RANDOM', `${pickRandom(['.dare2', '.truth3'])}`)
 
   }
-  if (/^dare$/i.test(command)) {
+  if (/^dare2$/i.test(command)) {
     let res = await fetch(global.API('pencarikode', '/api/dareid', {}, 'apikey'))
     if (!res.ok) throw await `${res.status} ${res.statusText}`
     let json = await res.json()
     if (json.message == "") throw json
-    conn.send3Button(m.chat, json.message, '', 'TRUTH', ',truth', 'DARE', ',dare', 'RANDOM', `${pickRandom([',dare', ',truth'])}`)
+    conn.send3Button(m.chat, json.message, '', 'TRUTH', '.truth3', 'DARE', '.dare2', 'RANDOM', `${pickRandom(['.dare2', '.truth3'])}`)
 
   }
 }
-handler.help = ['tod','truthordare']
+handler.help = ['tod','truthordare', 'truth3', 'dare2']
 handler.tags = ['fun']
 handler.command = /^(tod|truthordare)$/i
 
