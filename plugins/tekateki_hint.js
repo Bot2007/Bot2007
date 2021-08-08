c93fe44f1fb3cb5ebae271c1ad9f6166ac453825
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-09 05:17:46 +0800

    Update tekateki_hint.js

--- plugins/tekateki_hint.js
+++ plugins/tekateki_hint.js
@@ -7,6 +7,6 @@ let handler = async (m, { conn }) => {
     let clue = ans.replace(/[AIUEOaiueo]/g, '_')
     conn.reply(m.chat, '```' + clue + '```\nBalas soalnya, bukan pesan ini', conn.tekateki[id][0])
 }
-handler.command = /^tete$/i
+handler.command = ['tete']
 handler.limit = true
 module.exports = handler
