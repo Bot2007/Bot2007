142f93b4169597f2af01a66d0e09d6fdc3a9724f
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-10 20:15:10 +0800

    Update _premium.js

--- plugins/_premium.js
+++ plugins/_premium.js
@@ -2,12 +2,10 @@ let handler = m => m
 handler.before = async (m, { conn }) => {
     if (!m.isGroup && global.DATABASE.data.chats[m.chat].premium != 0) {
         if (new Date() * 1 >= global.DATABASE.data.chats[m.chat].premium) {
-            let users = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
             let index = global.prems.findIndex(v => (v.replace(/[^0-9]/g, '') + '@s.whatsapp.net') === (m.chat.replace(/[^0-9]/g, '') + '@s.whatsapp.net'))
-    conn.reply(m.chat, `Masa premium kamu sudah berakhir\n*Chat owner jika ingin premium lagi*`, null, { contextInfo: { mentionedJid: users } }).then(() => { global.prems.splice(index, 1).then(() => {
+            conn.reply(m.chat, `Masa premium kamu sudah berakhir\n*Chat owner jika ingin premium lagi*`, null).then(() => {
                         global.DATABASE.data.chats[m.chat].premium = 0
                     })
-                    })    
         }
     }
 }
