353e03d457cd8daa07adf4fb8fa8729b9ab85e93
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-12 07:39:32 +0800

    Update exec.js

--- plugins/exec.js
+++ plugins/exec.js
@@ -2,7 +2,7 @@ let syntaxerror = require('syntax-error')
 let util = require('util')
 
 let handler  = async (m, _2) => {
-  let { conn, usedPrefix, command, text, noPrefix, args, groupMetadata } = _2
+  let { conn, usedPrefix, command, participants, text, noPrefix, args, groupMetadata } = _2
   let _return
   let _syntax = ''
   let _text = (/^=/.test(usedPrefix) ? 'return ' : '') + noPrefix
@@ -12,7 +12,7 @@ let handler  = async (m, _2) => {
     let f = {
       exports: {}
     }
-    let exec = new (async () => {}).constructor('print', 'm', 'handler', 'require', 'conn', 'Array', 'process', 'args', 'groupMetadata', 'module', 'exports', 'argument', _text)
+    let exec = new (async () => {}).constructor('print', 'm', 'participants', 'handler', 'require', 'conn', 'Array', 'process', 'args', 'groupMetadata', 'module', 'exports', 'argument', _text)
     _return = await exec.call(conn, (...args) => {
       if (--i < 1) return
       console.log(...args)
