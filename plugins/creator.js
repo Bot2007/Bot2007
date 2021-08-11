909bdd4fdee124ef4f9d31662e211d9ae5fe19bf
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-11 19:03:46 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,18 +1,7 @@
 let fs = require ('fs')
 let handler  = async (m, { from, conn }) => {
 const fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: `status@broadcast`, }, message: { "contactMessage": { "displayName": `Taufik Owner/Botku`, "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:XL;Taufik,;;;\nFN:Taufik Bot,\nitem1.TEL;waid=601173093564:601173093564\nitem1.X-ABLabel:Ponsel\nEND:VCARD`, "jpegThumbnail": fs.readFileSync('./src/TaufikBot.png')}}}
-  let res = conn.relayWAMessage(conn.prepareMessageFromContent(m.chat, {
-                    "contactsArrayMessage": {
-          "displayName": "‎Ownerku",
-          "contacts": [
-            {
-              "displayName": "Taufik",
-              "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;Taufik(Bot);;;\nFN:Taufik(Bot)\nitem1.TEL;waid=601173093564:+60 11-7309 3564\nitem1.X-ABLabel:Telefon\nEND:VCARD"
-            }
-          ], quoted: fkon
-                    }
-                 }, {}), {waitForAck: true})
-           m.reply(`Ini ownerku yang tercinta`.trim(), null, { contextInfo: { mentionedJid: m.sender }, quoted: res})
+  conn.send2Contact(m.chat, '601173093564', 'Taufik', '0', 'Whatsapp', m, null, { contextInfo: { mentionedJid: m.sender }, quoted: fkon }).then((res) => m.reply(`Ini ownerku yang tercinta`.trim(), null, { contextInfo: { mentionedJid: m.sender }, quoted: res}))
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
