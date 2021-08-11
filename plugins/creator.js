275751cd0ee88695a92c8d096d60fed360812e39
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-11 18:42:44 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 let fs = require ('fs')
 let handler  = async (m, { from, conn }) => {
 const fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: `status@broadcast`, }, message: { "contactMessage": { "displayName": `Taufik Owner/Botku`, "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:XL;Taufik,;;;\nFN:Taufik Bot,\nitem1.TEL;waid=601173093564:601173093564\nitem1.X-ABLabel:Ponsel\nEND:VCARD`, "jpegThumbnail": fs.readFileSync('./src/TaufikBot.png')}}}
-conn.relayWAMessage(conn.prepareMessageFromContent(m.chat, {
+  let res = conn.relayWAMessage(conn.prepareMessageFromContent(m.chat, {
                     "contactsArrayMessage": {
           "displayName": "‎Ownerku",
           "contacts": [
@@ -11,7 +11,8 @@ conn.relayWAMessage(conn.prepareMessageFromContent(m.chat, {
             }
           ], quoted: fkon
                     }
-                 }, {}), {waitForAck: true}).then((res) => m.reply(`Ini ownerku yang tercinta`.trim(), null, { contextInfo: { mentionedJid: m.sender }, quoted: res}))
+                 }, {}), {waitForAck: true})
+           m.reply(`Ini ownerku yang tercinta`.trim(), null, { contextInfo: { mentionedJid: m.sender }, quoted: res})
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
