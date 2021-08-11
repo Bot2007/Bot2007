2d544410b38dad9161701b7e7e159ef6bc85e9e0
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-11 18:38:42 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -3,15 +3,11 @@ let handler  = async (m, { from, conn }) => {
 const fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: `status@broadcast`, }, message: { "contactMessage": { "displayName": `Taufik Owner/Botku`, "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:XL;Taufik,;;;\nFN:Taufik Bot,\nitem1.TEL;waid=601173093564:601173093564\nitem1.X-ABLabel:Ponsel\nEND:VCARD`, "jpegThumbnail": fs.readFileSync('./src/TaufikBot.png')}}}
 conn.relayWAMessage(conn.prepareMessageFromContent(m.chat, {
                     "contactsArrayMessage": {
-          "displayName": "‎2 kenalan",
+          "displayName": "‎Ownerku",
           "contacts": [
             {
-              "displayName": "+18654655888",
-              "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;+18654655888;;;\nFN:+18654655888\nitem1.TEL;waid=18654655888:+1 (865) 465-5888\nitem1.X-ABLabel:Telefon\nEND:VCARD"
-            },
-            {
               "displayName": "Taufik",
-              "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;Taufik;;;\nFN:Taufik\nitem1.TEL;waid=601110910716:+60 11-1091 0716\nitem1.X-ABLabel:Telefon\nEND:VCARD"
+              "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;Taufik(Bot);;;\nFN:Taufik(Bot)\nitem1.TEL;waid=601173093564:+60 11-7309 3564\nitem1.X-ABLabel:Telefon\nEND:VCARD"
             }
           ],
                         "contextInfo": { "stanzaId": m.key.id,
