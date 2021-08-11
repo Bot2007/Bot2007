5408e6e3e7c21ee141cb0110b99939802535a66f
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-12 03:05:01 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -200,7 +200,13 @@ ${ucapanWaktuMY}(Malaysia)
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).join`|`})`, 'g'), (_, name) => replace[name])
     let mentionedJid = [global.conn.user.jid]
-    m.reply(text.trim(), null, { thumbnail: fs.readFileSync('./src/TaufikBot.png'), contextInfo: { mentionedJid, forwardingScore: 508, isForwarded: true }, quoted: ftoko})
+    m.reply(text.trim(), null,  { thumbnail: require('fs').readFileSync('./src/TaufikBot.png'), contextInfo: { mentionedJid, forwardingScore: 508, isForwarded: true, "externalAdReply": {
+                  "title": "Taufik Bot",
+                  "body": "MENU TAUFIK BOT",
+                  "thumbnailUrl": "https://i.ibb.co/0tbBShq/IMG-20210705-WA0886.jpg",
+                  "mediaUrl": "https://youtu.be/dQw4w9WgXcQ",
+                  "thumbnail": ""
+                } }, quoted: ftoko})
 } catch (e) {
   TaufikComel.reply(m.chat, 'Menu Sedang Erorr!', m)
     throw e
