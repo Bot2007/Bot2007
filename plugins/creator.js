28e2d476218038d585fca9c98c59205f07b8caed
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-11 20:16:29 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let fs = require ('fs')
 let handler  = async (m, { from, conn }) => {
-const fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: `status@broadcast`, }, message: { "contactMessage": { "displayName": `Taufik Owner/Botku`, "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:XL;Taufik,;;;\nFN:Taufik Bot,\nitem1.TEL;waid=601173093564:601173093564\nitem1.X-ABLabel:Ponsel\nEND:VCARD`, "jpegThumbnail": fs.readFileSync('./src/TaufikBot.png')}}}
+const fkon = { key: { id: m.key.id, fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: `status@broadcast`, }, message: { "contactMessage": { "displayName": `Taufik Owner/Botku`, "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:XL;Taufik,;;;\nFN:Taufik Bot,\nitem1.TEL;waid=601173093564:601173093564\nitem1.X-ABLabel:Ponsel\nEND:VCARD`, "jpegThumbnail": fs.readFileSync('./src/TaufikBot.png')}}}
   let Yoko = conn.prepareMessageFromContent(m.chat, {
                     "contactsArrayMessage": {
           "displayName": "‎Ini ownerku ya tolol",
@@ -8,12 +8,12 @@ const fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid:
             {
               "displayName": "Taufik(Owner)",
               "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;Taufik(Owner);;;\nFN:Taufik(Owner)\nitem1.TEL;waid=601173093564:+60 11-7309 3564\nitem1.X-ABLabel:Telefon\nEND:VCARD"
+            },
+            {
+              "displayName": "WhatsApp",
+              "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;WhatsApp;;;\nFN:WhatsApp\nitem1.TEL;waid=0:+0\nitem1.X-ABLabel:Telefon\nEND:VCARD"
             }
-          ],
-                        "contextInfo": { "stanzaId": m.key.id,
-"participant": m.sender,
-"quotedMessage": m.message
-}
+          ]
                     }
                  }, { quoted: fkon })
           conn.relayWAMessage(Yoko, {waitForAck: true})
