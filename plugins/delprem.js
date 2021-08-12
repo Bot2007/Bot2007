05f5983c2d2686a48f7c4106ae156bd1d7cea981
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-13 03:23:14 +0800

    Update delprem.js

--- plugins/delprem.js
+++ plugins/delprem.js
@@ -4,6 +4,9 @@ let handler = async (m, { conn }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : ''
     else who = m.chat
     if (global.prems.includes(who)) throw 'Yang kamu tag bukan user premium!'
+    if (global.DATABASE.data.chats[m.chat].premium > 0) {
+        global.DATABASE.data.chats[m.chat].premium = 0
+    }
     let index = global.prems.findIndex(v => (v.replace(/[^0-9]/g, '') + '@s.whatsapp.net') === (who.replace(/[^0-9]/g, '') + '@s.whatsapp.net'))
     global.prems.splice(index, 1)
     conn.reply(m.chat, `Hai, @${who.split('@')[0]}. Masa premium kamu sudah berakhir!`, m, {
