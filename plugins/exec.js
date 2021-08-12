61326968c11afa9519968652afbcbd7e14180ebd
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-13 06:00:22 +0800

    Update exec.js

--- plugins/exec.js
+++ plugins/exec.js
@@ -2,7 +2,7 @@ let syntaxerror = require('syntax-error')
 let util = require('util')
 
 let handler  = async (m, _2) => {
-  let { conn, usedPrefix, from, command, participants, text, noPrefix, args, groupMetadata } = _2
+  let { conn, usedPrefix, command, participants, text, noPrefix, args, groupMetadata } = _2
   let _return
   let _syntax = ''
   let _text = (/^=/.test(usedPrefix) ? 'return ' : '') + noPrefix
@@ -12,12 +12,12 @@ let handler  = async (m, _2) => {
     let f = {
       exports: {}
     }
-    let exec = new (async () => {}).constructor('print', 'm', 'from', 'participants', 'handler', 'require', 'conn', 'Array', 'process', 'args', 'groupMetadata', 'module', 'exports', 'argument', _text)
+    let exec = new (async () => {}).constructor('print', 'm', 'args', 'participants', 'handler', 'require', 'conn', 'Array', 'process', 'args', 'groupMetadata', 'module', 'exports', 'argument', _text)
     _return = await exec.call(conn, (...args) => {
       if (--i < 1) return
       console.log(...args)
       return conn.reply(m.chat, util.format(...args), m)
-    }, m, from, participants, handler, require, conn, CustomArray, process, args, groupMetadata, f, f.exports, [conn, _2])
+    }, m, args, participants, handler, require, conn, CustomArray, process, args, groupMetadata, f, f.exports, [conn, _2])
   } catch (e) {
     let err = await syntaxerror(_text, 'Execution Function', {
       allowReturnOutsideFunction: true,
