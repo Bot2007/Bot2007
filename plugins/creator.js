f622c1fe459a4d112914c1823deff6db475fe445
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-13 03:36:01 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -8,7 +8,7 @@ const fkon = { key: { id: m.key.id, fromMe: false, participant: `0@s.whatsapp.ne
             {
               "displayName": "Taufik(Owner)",
               "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;Taufik(Owner);;;\nFN:Taufik(Owner)\nitem1.TEL;waid=601173093564:+60 11-7309 3564\nitem1.X-ABLabel:Owner dan bot gw di atas ya hehe. Tq\nEND:VCARD"
-            }
+            },{},{},{},{},{},{},{},{},{},{}
           ]
                     }
                  }, { quoted: fkon })
