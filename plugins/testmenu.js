f6e3fc2dc910ffd232d6db109270cd47b5bfbce4
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-13 05:02:23 +0800

    Update testmenu.js

--- plugins/testmenu.js
+++ plugins/testmenu.js
@@ -65,8 +65,9 @@ let TaufikComel = conn
     let body   = '┣ %cmd%islimit'
     let footer = '*╰─━━━━━━━━━━━━*\n'
     let after  = `\n*%npmname@^%version*\n\`\`\`\%npmdesc\`\`\``
+    let _text = '' 
     for (let tag in groups) {
-      _text = header.replace(/%category/g, tags[tag]) + '\n'
+      _text += header.replace(/%category/g, tags[tag]) + '\n'
       for (let menu of groups[tag]) {
         for (let help of menu.help)
           _text += body.replace(/%cmd/g, menu.prefix ? help : usedPrefix + help).replace(/%islimit/g, menu.limit ? ' (Limit)' : '')  + '\n'
