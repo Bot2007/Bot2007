b7144395c1c24a213bd5d4ac72279e6e8883c6bb
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-14 05:17:05 +0800

    Update addpremvip.js

--- plugins/addpremvip.js
+++ plugins/addpremvip.js
@@ -3,8 +3,9 @@ let handler = async (m, { conn }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (global.prems.includes(who.split`@`[0]) &&) throw 'Dia sudah premium vip!'
-    if (global.prems.includes(who.split`@`[0])) throw 'Dia sudah premium vip!'
+    if (global.prems.includes(m.mentionedJid[0].split`@`[0]) && m.isGroup) throw 'Dia sudah premium vip!'
+    if (global.prems.includes(m.chat.split`@`[0]) && global.DATABASE.data.chats[m.chat].premium = 0 && m.isPrivate) throw 'Dia sudah premium vip!'
+    if (global.prems.includes(m.chat.split`@`[0]) && global.DATABASE.data.chats[m.chat].premium > 0 && m.isPrivate) throw 'Dia melanggani premium berluput. Ketik .cekpremium untuk melihat kapan premium dia luput.'
     global.prems.push(`${who.split`@`[0]}`)
     conn.reply(m.chat, `Hai, @${who.split`@`[0]}. Kamu sudah premium!`, m, {
         contextInfo: {
