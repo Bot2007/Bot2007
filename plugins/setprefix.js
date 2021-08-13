77f78fdd543d369bf7d87e7cc4e169a18add3204
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-14 02:45:43 +0800

    Update setprefix.js

--- plugins/setprefix.js
+++ plugins/setprefix.js
@@ -1,5 +1,5 @@
 let handler = async(m, { conn, text }) => {
-  if (!text) { 
+  if (!text) return { 
   global.prefix = new RegExp('^[' + (opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
   m.reply(`Prefix berhasil direset`) }
   global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
