f23116683bee0df29efbef1d38fd2718b0e3bb8c
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-13 09:22:18 +0800

    Update handler.js

--- handler.js
+++ handler.js
@@ -503,11 +503,11 @@ Untuk mematikan fitur ini, ketik
       case 'mycontact':
         if (from in this.contacts && 'short' in this.contacts[from])
           return
-    user.call += 1
         break
     }
-    await this.sendMessage(from, `Jika kamu menelepon lebih dari 5, kamu akan diblokir.\n\n${user.call} / 5`,MessageType.extendedText)
-    if (user.call = 5) {
+    user.call += 1
+    await this.reply(from, `Jika kamu menelepon lebih dari 5, kamu akan diblokir.\n\n${user.call} / 5`, null)
+    if (user.call == 5) {
       await this.blockUser(from, 'add')
       user.call = 0
     }
