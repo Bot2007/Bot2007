64f708d2203ff2e1dd5bd650bb4053292570a67f
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-14 01:52:42 +0800

    Update jadilokasi.js

--- plugins/jadilokasi.js
+++ plugins/jadilokasi.js
@@ -5,15 +5,15 @@ let handler = async (m, { conn }) => {
   if (/image|webp/.test(mime)) {
     let img = await q.download()
     if (!img) throw 'Foto/Sticker tidak ditemukan'
-    conn.sendMessage(m.chat, {degreesLatitude: 25.21465, degreesLongitude: 49.33476, jpegThumbnail: img, Image: img, caption: "DZX NGEBUG:V"}, MessageType.location)
+    conn.sendMessage(m.chat, {degreesLatitude: 25.21465, degreesLongitude: 49.33476, jpegThumbnail: img, Image: img, caption: "Nih lokasi buat lo"}, require('@adiwajshing/baileys').location).then((ok) =>
   } else m.reply('Tag foto/stickernya!!')
   }  catch (e) {
     m.reply('*Hanya foto ama sticker aja bisa dijadikan lokasi.*')
   }
 }
-
-handler.help = ['jadilokasi']
+handler.help = ['video','image'].map(v => v + '2lokasi')
 handler.tags = ['tools']
-handler.command = /^(jadilokasi)$/i
+handler.command = /^(video|image)(2lokasi)$/i
+
 
 module.exports = handler
