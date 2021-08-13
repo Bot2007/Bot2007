c48ebf7e1308e37b644bd3b1fc360dcea2894618
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-14 01:47:58 +0800

    Update jadilokasi.js

--- plugins/jadilokasi.js
+++ plugins/jadilokasi.js
@@ -1,19 +1,15 @@
-/*
-* Fitur Image To Maps
-* Bukan fitur virus!!!
-* Hanya work di wa ori , wa gb ngebug gambar nya :v
-* Creator idhamthoriqbot
-* Recode by BochilGaming :)
-*/
-
 let handler = async (m, { conn }) => {
   let q = m.quoted ? m.quoted : m 
   let mime = (q.msg || q).mimetype || ''
-  if (/image|video/.test(mime)) {
+  try { 
+  if (/image|webp/.test(mime)) {
     let img = await q.download()
     if (!img) throw 'Foto/Sticker tidak ditemukan'
     conn.sendMessage(m.chat, {degreesLatitude: 25.21465, degreesLongitude: 49.33476, jpegThumbnail: img, Image: img, caption: "DZX NGEBUG:V"}, MessageType.location)
-  } else m.reply('Tag foto/videonya!!')
+  } else m.reply('Tag foto/stickernya!!')
+  }  catch (e) {
+    m.reply('*Hanya foto ama sticker aja bisa dijadikan lokasi.*')
+  }
 }
 
 handler.help = ['jadilokasi']
