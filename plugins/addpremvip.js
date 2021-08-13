ba3a5af6db0bc25ecdc4cb9e9d4ec0101f3c1835
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-14 05:22:57 +0800

    Update addpremvip.js

--- plugins/addpremvip.js
+++ plugins/addpremvip.js
@@ -1,11 +1,11 @@
-let handler = async (m, { conn }) => {
+let handler = async (m, { conn, usedPrefix }) => {
   
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (global.prems.includes(m.mentionedJid[0].split`@`[0]) && m.isGroup) throw 'Dia sudah premium vip!'
-    if (global.prems.includes(m.chat.split`@`[0]) && global.DATABASE.data.chats[m.chat].premium = 0 && m.isPrivate) throw 'Dia sudah premium vip!'
-    if (global.prems.includes(m.chat.split`@`[0]) && global.DATABASE.data.chats[m.chat].premium > 0 && m.isPrivate) throw 'Dia melanggani premium berluput. Ketik .cekpremium untuk melihat kapan premium dia luput.'
+    if (global.prems.includes(m.mentionedJid[0].split`@`[0]) && m.isGroup) throw '${conn.getName(who)} sudah premium vip!'
+    if (global.prems.includes(m.chat.split`@`[0]) && global.DATABASE.data.chats[m.chat].premium = 0 && m.isPrivate) throw '${conn.getName(who)} sudah premium vip!'
+    if (global.prems.includes(m.chat.split`@`[0]) && global.DATABASE.data.chats[m.chat].premium > 0 && m.isPrivate) throw '${conn.getName(who)} melanggani premium berluput. Ketik ${usedPrefix}cekpremium untuk melihat kapan premium ${conn.getName(who)} luput.'
     global.prems.push(`${who.split`@`[0]}`)
     conn.reply(m.chat, `Hai, @${who.split`@`[0]}. Kamu sudah premium!`, m, {
         contextInfo: {
