c5300827d6c12a7c6b204a826137d4573d536d31
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-14 06:32:55 +0800

    Update _expired.js

--- plugins/_expired.js
+++ plugins/_expired.js
@@ -2,8 +2,7 @@ let handler = m => m
 handler.before = async (m, { conn }) => {
     if (m.isGroup && global.DATABASE.data.chats[m.chat].expired != 0) {
         if (new Date() * 1 >= global.DATABASE.data.chats[m.chat].expired) {
-            let users = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
-            conn.reply(m.chat, `waktunya *${conn.user.name}* untuk meninggalkan grup :(\n*Chat owner jika ingin sewa bot lagi*`, null, { contextInfo: { mentionedJid: users } }).then(() => {
+            conn.reply(m.chat, `Waktunya *${conn.user.name}* untuk meninggalkan grup :(\n*Chat owner jika ingin sewa bot lagi*`, null, null, { contextInfo: { mentionedJid: users } }).then(() => {
                 conn.sendContact(m.chat, '601173093564', 'Taufik', m).then(() => {
                     conn.groupLeave(m.chat).then(() => {
                         global.DATABASE.data.chats[m.chat].expired = 0
