8afdd2cd99fb9d3a2be8873bdb18f03daf12a13c
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-14 05:01:40 +0800

    Update addpremvip.js

--- plugins/addpremvip.js
+++ plugins/addpremvip.js
@@ -3,7 +3,8 @@ let handler = async (m, { conn }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (global.prems.includes(who.split`@`[0])) throw 'Sudah premium!'
+    if (global.prems.includes(who.split`@`[0]) &&) throw 'Dia sudah premium vip!'
+    if (global.prems.includes(who.split`@`[0])) throw 'Dia sudah premium vip!'
     global.prems.push(`${who.split`@`[0]}`)
     conn.reply(m.chat, `Hai, @${who.split`@`[0]}. Kamu sudah premium!`, m, {
         contextInfo: {
