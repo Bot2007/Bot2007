ec836d6fe28d489b57465d5e0d0e4a64982710a9
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-14 23:10:29 +0800

    Update _tagowner.js

--- plugins/_tagowner.js
+++ plugins/_tagowner.js
@@ -1,7 +1,7 @@
 let handler = m => m
 handler.before = async (m, { conn }) => {
     if (m.isGroup) {
-    if (m.mentionedJid == [global.conn.user.jid]) {
+    if (m.mentionedJid == global.conn.user.jid) {
     m.reply(`Apa sih tag tag ownerku`.trim(), null, { thumbnail: require('fs').readFileSync('./src/TaufikBot.png')})
 }
         
