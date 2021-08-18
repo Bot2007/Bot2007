5d05d20c266e52aa82df6ced1a37a3bd00263e86
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-18 18:45:55 +0800

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -24,13 +24,7 @@ let handler = async (m, { conn, command, text, isPrems, isOwner }) => {
   if (yt === false) throw 'Semua server tidak bisa :/'
   let { dl_link, thumb, title, filesize, filesizeF } = yt
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*Source:* ${vid.url}
-*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
-*Server y2mate:* ${usedServer}
-`.trim(), m)
+  m.reply('Sebentar ya, lagu/video mu sedang di hantar')
 let _thumb = {}
 try { if (isVideo) _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
 catch (e) { }
@@ -43,7 +37,7 @@ if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(comm
   asDocument: chat.useDocument,
   contextInfo: { forwardingScore: 508, isForwarded: true, "externalAdReply": {
                   "title": title,
-                  "body": filesizeF,
+                  "body": "Filesize: " + filesizeF,
                   "mediaType": "VIDEO",
                   "thumbnailUrl": thumb,
                   "thumbnail": "",
