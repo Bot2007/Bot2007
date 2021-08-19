c30fd8006d93bac0cc1aefb5404125efcfaf481f
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-20 07:07:51 +0800

    Update unknown.js

--- plugins/unknown.js
+++ plugins/unknown.js
@@ -1,1 +1,1 @@
-module.exports = async global.fakethumb = await (await require('node-fetch')('https://i.ibb.co/0tbBShq/IMG-20210705-WA0886.jpg')).buffer()
+module.exports = async (m, { conn }) => global.fakethumb = await (await require('node-fetch')('https://i.ibb.co/0tbBShq/IMG-20210705-WA0886.jpg')).buffer()
