602efa879e926a5ca43e5bcc7e61f4e1ccace246
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-20 07:46:36 +0800

    Update main.js

--- main.js
+++ main.js
@@ -153,6 +153,10 @@ global.reloadHandler = function () {
   isInit = false
   return true
 }
+// Fake thumb
+async () => {
+global.fakethumb = await (await require('node-fetch')('https://i.ibb.co/0tbBShq/IMG-20210705-WA0886.jpg')).buffer()
+}
 
 // Plugin Loader
 let pluginFolder = path.join(__dirname, 'plugins')
