be1fb45ea78dea3152cd2bee5e360183c2abdff9
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-20 06:22:41 +0800

    Update hentai2.js

--- plugins/hentai2.js
+++ plugins/hentai2.js
@@ -2,9 +2,9 @@ let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
 let url = await fetch('https://waifu.pics/api/nsfw/neko')
   let hentai = await url.json()
-  let hasil = `Link ${hentai.url}`
+  let hasil = `Tobat bro`
  // conn.fakeReply(m.chat, '*_Sabar sedang mengirim hentai..._*','0@s.whatsapp.net','Orang sabar di sayang mantan:)')
-conn.sendFile(m.chat, hentai.url, 'hentai.png', hasil, m, false, { thumbnail: require('fs').readFileSync('src/TaufikBot.png') } )
+conn.sendFile(m.chat, hentai.url, 'hentai.png', hasil, m)
 }
 handler.command = ['hentai2']
 handler.register = true
