b533502832acc18c8cc2673e3905b7c94420cbe3
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-20 07:51:15 +0800

    Update main.js

--- main.js
+++ main.js
@@ -154,7 +154,7 @@ global.reloadHandler = function () {
   return true
 }
 // Fake thumb
-async => {
+async m => {
 global.fakethumb = await (await require('node-fetch')('https://i.ibb.co/0tbBShq/IMG-20210705-WA0886.jpg')).buffer()
 }
 
