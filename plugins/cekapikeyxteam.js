f058d16c813963d08a797cd0196cbca2b118bcc4
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-23 04:11:14 +0800

    Update cekapikeyxteam.js

--- plugins/cekapikeyxteam.js
+++ plugins/cekapikeyxteam.js
@@ -5,13 +5,13 @@ let handler = async (m, { text }) => {
   let res = await fetch('https://api.xteam.xyz/cekey?APIKEY=' + text)
   let json = await res.json()
   if (json.status) m.reply(`
-  IP = ${json.response.ip}
-  Nama pendaftar apikey = ${json.response.name}
-  Email pendaftar apikey = ${json.response.email}
-  Total hit = ${json.response.totalhit}
-  Premium = ${json.response.premium == false ? "Tidak" : "Ya"}
-  
-  ${json.response.expired}`.trim())
+Alamat IP = ${json.response.ip}
+Nama pendaftar apikey = ${json.response.name}
+Email pendaftar apikey = ${json.response.email}
+Total hit = ${json.response.totalhit}
+Premium = ${json.response.premium == false ? "Tidak" : "Ya"}
+
+${json.response.premium == false ? " " : "Expired =" + json.response.expired}`.trim())
   else throw 'Apikey invalid'
 }
 handler.help = ['cekapikeyxteam'].map(v => v + ' <apikey>')
