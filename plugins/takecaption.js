f2b9269ba9cd7612d3d0c78d7314f07598638e66
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-23 18:25:11 +0800

    Update takecaption.js

--- plugins/takecaption.js
+++ plugins/takecaption.js
@@ -3,7 +3,7 @@ let handler = async (m, { usedPrefix, command }) => {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
     if (!mime) throw `ini tu gunanya buat ngambil caption yang ada digambar, kirim/balas gambar dengan perintah ${usedPrefix + command}`
-    if (/image\/(jpe?g|png)/.test(mime)) return m.reply((q.msg || q).caption)
+    if (/(image|video)\/(jpe?g|png)/.test(mime)) return m.reply((q.msg || q).caption)
     else throw 'Gak ada caption'
 }
 handler.help = ['takecaption']
