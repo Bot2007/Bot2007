418c3d37aea6a9383919176914775966d0e7eed2
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-23 18:31:59 +0800

    Update takecaption.js

--- plugins/takecaption.js
+++ plugins/takecaption.js
@@ -3,10 +3,12 @@ let handler = async (m, { usedPrefix, command }) => {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
     if (!mime) throw `ini tu gunanya buat ngambil caption yang ada digambar, kirim/balas gambar dengan perintah ${usedPrefix + command}`
-    if (/(image|video)\/(jpe?g|png)/.test(mime)) return m.reply((q.msg || q).caption)
-    else throw 'Gak ada caption'
+    if (!(q.msg || q).caption) throw `Gak ada caption`
+    if (/image\/(jpe?g|png)/.test(mime)) return m.reply((q.msg || q).caption)
+    else if (/video/.test(mime)) return m.reply((q.msg || q).caption)
+    else throw 'Format tidak mendukung'
 }
-handler.help = ['takecaption']
+handler.help = ['takecaption'].map(v => v + '<video|image>')
 handler.tags = ['tools']
 handler.command = /^takecaption$/i
 handler.limit = true
