2ee81190e5bcddb44db1042310505ed3ecea8308
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-24 00:14:28 +0800

    Delete abc.js

--- plugins/abc.js
+++
@@ -1,7 +0,0 @@
-let handler = m => m
-handler.before = async (m, { conn }) => {
-if (!global.fakethumb) {
-global.fakethumb = await (await require('node-fetch')('https://i.ibb.co/0tbBShq/IMG-20210705-WA0886.jpg')).buffer()
-} else return
-}
-module.exports = handler
