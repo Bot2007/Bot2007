a94a71c7b91a9bf1b75c68c5bcecd72aa27d481e
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-24 00:19:50 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,7 +2,7 @@ let handler = async m => m.reply(`
 ╭─「 Donasi 」
 │ • Saweria = https://bit.ly/35L1whO
 ╰────
-`.trim(), null, { thumbnail: global.fakethumb }).then(() => m.reply('${global.fakethumb}'))
+`.trim(), null, { thumbnail: global.fakethumb })
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
