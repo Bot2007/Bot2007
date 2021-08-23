660b1fb278d2666747ea94282babb3fca0156e41
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-24 00:12:27 +0800

    KO

--- plugins/abc.js
+++ plugins/abc.js
@@ -1,7 +1,7 @@
 let handler = m => m
 handler.before = async (m, { conn }) => {
-if (!global.fakethumb) continue
+if (!global.fakethumb) {
 global.fakethumb = await (await require('node-fetch')('https://i.ibb.co/0tbBShq/IMG-20210705-WA0886.jpg')).buffer()
-return
+} else return
 }
 module.exports = handler
