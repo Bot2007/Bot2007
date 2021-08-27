6d0a923fae555c1f929b4793272b138ef46982ab
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-27 16:59:41 +0800

    Update buttonmenu.js

--- plugins/buttonmenu.js
+++ plugins/buttonmenu.js
@@ -1,4 +1,5 @@
 let handler = async (m,{ conn }) => {
+            global.fakethumb = await (await require('node-fetch')('https://i.ibb.co/0tbBShq/IMG-20210705-WA0886.jpg')).buffer()
             conn.relayWAMessage(conn.prepareMessageFromContent(m.chat, {
                     "listMessage":  {
                         "title": "",
