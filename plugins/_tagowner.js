a2a0e4c799801949c22906172143bada994ec592
Bot2007 <87221182+Bot2007@users.noreply.github.com>
2021-08-28 11:07:51 +0800

    Update _tagowner.js

--- plugins/_tagowner.js
+++ plugins/_tagowner.js
@@ -1,8 +1,13 @@
 let handler = m => m
 handler.before = async (m, { conn }) => {
-    if (m.isGroup && m.mentionedJid == global.conn.user.jid) { 
-        m.reply(`Apa sih tag tag ownerku`.trim(), null, { thumbnail: global.fakethumb })
-    }
-    return
+    if (m.isGroup) {
+    let jids = [...new Set([...(m.mentionedJid || [])])]
+    for (let jid of jids) {
+    let user = global.DATABASE.data.users[jid].owner
+    if (!user) return
+    m.reply(`Apa sih tag tag ownerku`.trim(), null, { thumbnail: global.fakethumb })
+  }
+}
+  return
 }
 module.exports = handler
